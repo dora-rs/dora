147944a45b0d170b798b1571c54b0ebdcbc0e547
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2021-08-08 03:00:58 +0900

    Inlinezation of *_fini

--- rclrust/src/context.rs
+++ rclrust/src/context.rs
@@ -49,10 +49,6 @@ impl RclContext {
         }
         Ok(())
     }
-
-    unsafe fn fini(&mut self) -> anyhow::Result<()> {
-        rcl_sys::rcl_context_fini(self.0.as_mut()).to_result()
-    }
 }
 
 impl Drop for RclContext {
@@ -64,7 +60,7 @@ impl Drop for RclContext {
                 e
             )
         }
-        if let Err(e) = unsafe { self.fini() } {
+        if let Err(e) = unsafe { rcl_sys::rcl_context_fini(self.0.as_mut()).to_result() } {
             rclrust_error!(
                 Logger::new("rclrust"),
                 "Failed to clean up rcl context handle: {}",
