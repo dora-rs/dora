f95fa8c96519fc162bf0cfdd84f616e6512ee54e
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2021-08-22 17:26:53 +0900

    Enable test for rcl-sys

--- rcl-sys/src/lib.rs
+++ rcl-sys/src/lib.rs
@@ -4,6 +4,7 @@
     non_snake_case,
     non_upper_case_globals,
     invalid_codeblock_attributes,
+    deref_nullptr,
     clippy::all,
     rustdoc::bare_urls,
     rustdoc::broken_intra_doc_links,
