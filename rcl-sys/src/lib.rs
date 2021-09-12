c2b4a5c7b78c81096b23ffcf70bd949eb122deac
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2021-09-12 13:06:19 +0900

    invalid_codeblock_attributes -> rustdoc::invalid_codeblock_attributes

--- rcl-sys/src/lib.rs
+++ rcl-sys/src/lib.rs
@@ -1,13 +1,13 @@
 #![allow(
+    deref_nullptr,
     improper_ctypes,
     non_camel_case_types,
     non_snake_case,
     non_upper_case_globals,
-    invalid_codeblock_attributes,
-    deref_nullptr,
     clippy::all,
     rustdoc::bare_urls,
     rustdoc::broken_intra_doc_links,
+    rustdoc::invalid_codeblock_attributes,
     rustdoc::invalid_rust_codeblocks
 )]
 
