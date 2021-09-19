1362bf818c0cdd7b92899f37946a4498cdfd22e5
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2021-09-19 15:03:38 +0900

    Configure features

--- rcl-sys/build.rs
+++ rcl-sys/build.rs
@@ -1,4 +1,17 @@
+#[allow(unused_imports)]
+use std::env;
+
+#[cfg(not(any(feature = "foxy", feature = "galactic", feature = "rolling")))]
+compile_error!("Any distribution feature should be specified");
+
 fn main() {
+    #[cfg(feature = "foxy")]
+    assert_eq!(env::var("ROS_DISTRO"), Ok("foxy".to_string()));
+    #[cfg(feature = "galactic")]
+    assert_eq!(env::var("ROS_DISTRO"), Ok("galactic".to_string()));
+    #[cfg(feature = "rolling")]
+    assert_eq!(env::var("ROS_DISTRO"), Ok("rolling".to_string()));
+
     println!("cargo:rerun-if-env-changed=AMENT_PREFIX_PATH");
 
     let ament_prefix_paths =
