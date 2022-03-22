7b4408ec882f412acaf88fafcae71222ec5982af
haixuanTao <hai-xuan.tao@student.ecp.fr>
2022-03-22 13:54:04 +0100

    Reintroducing the pyo3 async tokio derive main
    Pyo3 async only work with a main derived from the proc macro pyo3
    asyncio main.
    
    If this is removed Python async call does not work.

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import asyncio
+
+
 async def return_1(x):
     print(x)
     return {"b": "b", "c": "c"}
--- src/server.rs
+++ src/server.rs
@@ -1,32 +1,69 @@
 use eyre::{eyre, Context};
 use futures::prelude::*;
 use pyo3::prelude::*;
-use std::collections::HashMap;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::{BTreeMap, HashMap};
 use std::env;
+use std::hash::Hash;
+use std::hash::Hasher;
+use std::time::Duration;
+use tokio::time::timeout;
 use zenoh::config::Config;
-use zenoh::net::protocol::io::SplitBuffer;
+use zenoh::prelude::SplitBuffer;
+static DURATION_MILLIS: u64 = 5;
 
-pub async fn start_server(file: &str, app: &str) -> eyre::Result<()> {
+#[pyo3_asyncio::tokio::main]
+pub async fn main() -> PyResult<()> {
     // Subscribe
     env_logger::init();
     let config = Config::default();
     let session = zenoh::open(config).await.unwrap();
-    let mut subscriber = session
-        .subscribe(env::var("SRC_LABELS").unwrap())
-        .await
-        .map_err(|err| {
-            eyre!("Could not subscribe to the given subscription key expression. Error: {err}")
-        })
+    let subscriptions = env::var("SRC_LABELS")
+        .wrap_err("Env variable not set")
         .unwrap();
-    let identity = initialize(file, app).await.unwrap();
+    let subscriptions = subscriptions.split(":");
+
+    let mut subscribers = HashMap::new();
+
+    for subscription in subscriptions {
+        subscribers.insert(subscription, session
+            .subscribe(subscription)
+            .await
+            .map_err(|err| {
+                eyre!("Could not subscribe to the given subscription key expression. Error: {err}")
+            })
+            .unwrap());
+    }
+
+    let mut states = BTreeMap::new();
+    let mut hasher = DefaultHasher::new();
+    states.hash(&mut hasher);
+    let mut state_hash = hasher.finish();
+
+    let identity = initialize("app".to_string(), "return_1".to_string()).unwrap();
+    let dur = Duration::from_millis(DURATION_MILLIS);
 
     loop {
-        let data = subscriber.next().await.unwrap().value.payload;
-        let binary = data.contiguous();
-        println!("recieved data");
+        for (subscription, subscriber) in subscribers.iter_mut() {
+            let result = timeout(dur, subscriber.next()).await;
+            if let Ok(Some(data)) = result {
+                let value = data.value.payload;
+                let binary = value.contiguous();
+                states.insert(subscription.clone(), binary.to_vec());
+            }
+        }
+
+        let mut hasher = DefaultHasher::new();
+        states.hash(&mut hasher);
+
+        if state_hash == hasher.finish() {
+            continue;
+        } else {
+            state_hash = hasher.finish();
+        }
 
         let result = Python::with_gil(|py| {
-            let args = (binary.into_py(py),);
+            let args = (states.clone().into_py(py),);
             pyo3_asyncio::tokio::into_future(
                 identity
                     .call(py, args, None)
@@ -56,10 +93,10 @@ pub async fn start_server(file: &str, app: &str) -> eyre::Result<()> {
     }
 }
 
-pub async fn initialize(file: &str, app: &str) -> eyre::Result<Py<PyAny>> {
+pub fn initialize(file: String, app: String) -> eyre::Result<Py<PyAny>> {
     Ok(Python::with_gil(|py| {
         let file = py
-            .import(file)
+            .import(&file)
             .wrap_err("The import file was not found. Check your PYTHONPATH env variable.")
             .unwrap();
         // convert Function into a PyObject
