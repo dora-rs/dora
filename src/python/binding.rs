b0d74791ad2490521eb36d9cc417f4971bac41a9
Philipp Oppermann <dev@phil-opp.com>
2022-03-23 11:45:01 +0100

    Don't panic in the Python binding functions

--- src/python/binding.rs
+++ src/python/binding.rs
@@ -4,18 +4,16 @@ use std::collections::{BTreeMap, HashMap};
 
 pub fn init(app: &str, function: &str) -> eyre::Result<Py<PyAny>> {
     pyo3::prepare_freethreaded_python();
-    Ok(Python::with_gil(|py| {
+    Python::with_gil(|py| {
         let file = py
             .import(app)
-            .wrap_err("The import file was not found. Check your PYTHONPATH env variable.")
-            .unwrap();
+            .wrap_err("The import file was not found. Check your PYTHONPATH env variable.")?;
         // convert Function into a PyObject
         let identity = file
             .getattr(function)
-            .wrap_err("The Function was not found in the imported file.")
-            .unwrap();
-        identity.to_object(py)
-    }))
+            .wrap_err("The Function was not found in the imported file.")?;
+        Ok(identity.to_object(py))
+    })
 }
 
 pub async fn call(
@@ -23,12 +21,12 @@ pub async fn call(
     states: BTreeMap<String, String>,
 ) -> eyre::Result<HashMap<String, String>> {
     Python::with_gil(|py| {
-        let args = (states.clone().into_py(py),);
+        let args = (states.into_py(py),);
         let result = py_function
             .call(py, args, None)
-            .wrap_err("The Python function call did not succeed.")
-            .unwrap();
-        result.extract(py)
+            .wrap_err("The Python function call did not succeed.")?;
+        result
+            .extract(py)
+            .wrap_err("The Python function returned an error.")
     })
-    .wrap_err("")
 }
