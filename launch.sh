94b715f175cb33d770c1610e37bec6d369ee0a4b
haixuanTao <hai-xuan.tao@student.ecp.fr>
2022-04-28 12:52:06 +0100

    Removing Dora-Pylot

--- launch.sh
+++
@@ -1,7 +0,0 @@
-nvidia-docker build --tag dora .
-nvidia-docker run -itd --name dora -p 20022:22  dora /bin/bash
-nvidia-docker exec -itd dora /home/erdos/workspace/pylot/scripts/run_simulator.sh
-nvidia-docker cp ~/.ssh/id_rsa.pub dora:/home/erdos/.ssh/authorized_keys
-nvidia-docker exec -i -t dora sudo chown erdos /home/erdos/.ssh/authorized_keys
-nvidia-docker exec -i -t dora sudo service ssh start
-ssh -p 20022 -X erdos@localhost
