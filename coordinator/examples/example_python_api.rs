aa9ac63d70d2f11126693428d445bc303c2d1590
haixuanTao <hai-xuan.tao@student.ecp.fr>
2022-05-27 15:39:20 +0200

    Improving example python api

--- coordinator/examples/example_python_api.rs
+++ coordinator/examples/example_python_api.rs
@@ -1,10 +1,11 @@
 use dora_api::{self, config::DataId, DoraNode};
-use eyre::{bail, Context};
+use eyre::Context;
 use futures::StreamExt;
 use std::{
     collections::BTreeMap,
     time::{Duration, Instant},
 };
+use tokio::time::sleep;
 
 static ATOMIC_TIMEOUT: Duration = Duration::from_millis(20);
 static BATCH_TIMEOUT: Duration = Duration::from_millis(100);
@@ -12,16 +13,16 @@ use dora_rs::python::binding::PythonBinding;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let Node = DoraNode::init_from_env().await?;
+    let node = DoraNode::init_from_env().await?;
 
-    let app = std::env::var("DORA_NODE_PYTHON_FILE")
-        .wrap_err("env variable DORA_NODE_RUN_CONFIG must be set")?;
+    let app_ = node.env;
     let func = std::env::var("DORA_NODE_PYTHON_FUNCTION")
-        .wrap_err("env variable DORA_NODE_RUN_CONFIG must be set")?;
+        .wrap_err("env variable DORA_NODE_PYTHON_FUNCTION must be set")?;
+    let is_source = std::env::var("IS_SOURCE").wrap_err("env variable IS_SOURCE must be set")?;
 
     let py_function = PythonBinding::try_new(&app, &func).unwrap();
 
-    let mut inputs = Node.inputs().await?;
+    let mut inputs = node.inputs().await?;
 
     loop {
         let mut workload = BTreeMap::new();
@@ -29,13 +30,17 @@ async fn main() -> eyre::Result<()> {
         let py_function = py_function.clone();
 
         // Retrieve several inputs within a time frame
-        while time_at_start.elapsed() < BATCH_TIMEOUT {
-            let input = match tokio::time::timeout(ATOMIC_TIMEOUT, inputs.next()).await {
-                Ok(Some(input)) => input,
-                Ok(None) => continue,
-                Err(_) => continue,
-            };
-            workload.insert(input.id.to_string(), input.data);
+        if is_source != "true" {
+            while time_at_start.elapsed() < BATCH_TIMEOUT {
+                let input = match tokio::time::timeout(ATOMIC_TIMEOUT, inputs.next()).await {
+                    Ok(Some(input)) => input,
+                    Ok(None) => continue,
+                    Err(_) => continue,
+                };
+                workload.insert(input.id.to_string(), input.data);
+            }
+        } else {
+            sleep(BATCH_TIMEOUT).await;
         }
 
         // Call the function
@@ -43,7 +48,7 @@ async fn main() -> eyre::Result<()> {
 
         // Send the data one by one.
         for (k, v) in batch_messages {
-            Node.send_output(&DataId::from(k), &v).await.unwrap();
+            node.send_output(&DataId::from(k), &v).await.unwrap();
         }
     }
 }
