a63deb54ec6f30b321f72e4f1d9e002dd5e0d780
Philipp Oppermann <dev@phil-opp.com>
2022-05-30 12:36:41 +0200

    Set `reliable` flag when subscribing to zenoh messages

--- zenoh-logger/src/main.rs
+++ zenoh-logger/src/main.rs
@@ -2,7 +2,7 @@ use zenoh::prelude::{Receiver, ZFuture};
 
 fn main() {
     let zenoh = zenoh::open(zenoh_config::Config::default()).wait().unwrap();
-    let mut sub = zenoh.subscribe("/**").wait().unwrap();
+    let mut sub = zenoh.subscribe("/**").reliable().wait().unwrap();
 
     loop {
         let msg = sub.receiver().recv().unwrap();
