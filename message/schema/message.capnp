d8cc8b608561523925c9f3a1858273e21cb3756d
haixuanTao <tao.xavier@outlook.com>
2022-06-10 16:21:32 +0200

    Remove `depth` from the message `metadata`

--- message/schema/message.capnp
+++ message/schema/message.capnp
@@ -4,8 +4,7 @@ struct Metadata {
   metadataVersion @0 :UInt16;
   watermark @1 :UInt64; 
   deadline @2 :UInt64;
-  otelContext @3 :Text;
-  depth @4 :UInt32; # The depth within the graph of the message 
+  otelContext @3 :Text; # OpenTelemetry Context allowing shared context between nodes.
 }
 
 
--- message/src/lib.rs
+++ message/src/lib.rs
@@ -5,12 +5,11 @@ pub mod message_capnp {
 }
 
 /// Helper function to serialize message
-pub fn serialize_message(data: &[u8], otel_context: &str, depth: u32) -> Vec<u8> {
+pub fn serialize_message(data: &[u8], otel_context: &str) -> Vec<u8> {
     // Build the metadata
     let mut meta_builder = capnp::message::Builder::new_default();
     let mut metadata = meta_builder.init_root::<message_capnp::metadata::Builder>();
     metadata.set_otel_context(otel_context);
-    metadata.set_depth(depth);
 
     // Build the data of the message
     let mut builder = capnp::message::Builder::new_default();
