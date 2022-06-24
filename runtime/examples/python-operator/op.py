d7e98e9f6718419b1679fdb7f0f9b982a80b1dc5
Philipp Oppermann <dev@phil-opp.com>
2022-06-24 14:24:02 +0200

    Remove `dora_` prefix for `on_input` and `drop_operator` methods

--- runtime/examples/python-operator/op.py
+++ runtime/examples/python-operator/op.py
@@ -2,10 +2,10 @@ class Operator:
     def __init__(self, counter=0):
         self.counter = counter
 
-    def dora_drop_operator(self):
+    def drop_operator(self):
         print('drop python operator')
     
-    def dora_on_input(self, id, value, send_output):
+    def on_input(self, id, value, send_output):
         val_len = len(value)
         print(f'PYTHON received input {id}; value length: {val_len}')
         send_output("counter", self.counter.to_bytes(1, 'little'))
