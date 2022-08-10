5d334a4c4da426aee2282de4bc4d7b8933d98f43
Philipp Oppermann <dev@phil-opp.com>
2022-08-10 12:39:57 +0200

    Add docs and safety annotations to the functions of the dora node C API

--- apis/c/node/src/lib.rs
+++ apis/c/node/src/lib.rs
@@ -9,6 +9,15 @@ struct DoraContext {
     inputs: Pin<Box<dyn futures::Stream<Item = Input>>>,
 }
 
+/// Initializes a dora context from the environment variables that were set by
+/// the dora-coordinator.
+///
+/// Returns a pointer to the dora context on success. This pointer can be
+/// used to call dora API functions that expect a `context` argument. Any
+/// other use is prohibited. To free the dora context when it is no longer
+/// needed, use the [`free_dora_context`] function.
+///
+/// On error, a null pointer is returned.
 #[no_mangle]
 pub extern "C" fn init_dora_context_from_env() -> *mut () {
     let context = match block_on(async {
@@ -28,6 +37,13 @@ pub extern "C" fn init_dora_context_from_env() -> *mut () {
     Box::into_raw(Box::new(context)).cast()
 }
 
+/// Frees the given dora context.
+///
+/// ## Safety
+///
+/// Only pointers created through [`init_dora_context_from_env`] are allowed
+/// as arguments. Each context pointer must be freed exactly once. After
+/// freeing, the pointer must not be used anymore.
 #[no_mangle]
 pub unsafe extern "C" fn free_dora_context(context: *mut ()) {
     let context: Box<DoraContext> = unsafe { Box::from_raw(context.cast()) };
@@ -37,6 +53,21 @@ pub unsafe extern "C" fn free_dora_context(context: *mut ()) {
     let _ = unsafe { Box::from_raw(node as *const DoraNode as *mut DoraNode) };
 }
 
+/// Waits for the next incoming input for the node.
+///
+/// Returns a pointer to the input on success. This pointer must not be used
+/// directly. Instead, use the `read_dora_input_*` functions to read out the
+/// ID and data of the input. When the input is not needed anymore, use
+/// [`free_dora_input`] to free it again.
+///
+/// Returns a null pointer when all input streams were closed. This means that
+/// no more input will be available. Nodes typically react by stopping.
+///
+/// ## Safety
+///
+/// The `context` argument must be a dora context created through
+/// [`init_dora_context_from_env`]. The context must be still valid, i.e., not
+/// freed yet.
 #[no_mangle]
 pub unsafe extern "C" fn dora_next_input(context: *mut ()) -> *mut () {
     let context: &mut DoraContext = unsafe { &mut *context.cast() };
@@ -46,6 +77,18 @@ pub unsafe extern "C" fn dora_next_input(context: *mut ()) -> *mut () {
     }
 }
 
+/// Reads out the ID of the given input.
+///
+/// Writes the `out_ptr` and `out_len` with the start pointer and length of the
+/// ID string of the input. The ID is guaranteed to be valid UTF-8.
+///
+/// ## Safety
+///
+/// The `input` argument must be a dora input received through
+/// [`dora_next_input`]. The input must be still valid, i.e., not
+/// freed yet. The returned `out_ptr` must not be used after
+/// freeing the `input`, since it points directly into the input's
+/// memory.
 #[no_mangle]
 pub unsafe extern "C" fn read_dora_input_id(
     input: *const (),
@@ -62,6 +105,19 @@ pub unsafe extern "C" fn read_dora_input_id(
     }
 }
 
+/// Reads out the data of the given input.
+///
+/// Writes the `out_ptr` and `out_len` with the start pointer and length of the
+/// input's data array. The data array is a raw byte array, whose format
+/// depends on the source operator/node.
+///
+/// ## Safety
+///
+/// The `input` argument must be a dora input received through
+/// [`dora_next_input`]. The input must be still valid, i.e., not
+/// freed yet. The returned `out_ptr` must not be used after
+/// freeing the `input`, since it points directly into the input's
+/// memory.
 #[no_mangle]
 pub unsafe extern "C" fn read_dora_input_data(
     input: *const (),
@@ -78,11 +134,35 @@ pub unsafe extern "C" fn read_dora_input_data(
     }
 }
 
+/// Frees the given dora input.
+///
+/// ## Safety
+///
+/// Only pointers created through [`dora_next_input`] are allowed
+/// as arguments. Each context pointer must be freed exactly once. After
+/// freeing, the pointer and all derived pointers must not be used anymore.
+/// This also applies to the `read_dora_input_*` functions, which return
+/// pointers into the original input structure.
 #[no_mangle]
 pub unsafe extern "C" fn free_dora_input(input: *mut ()) {
     let _: Box<Input> = unsafe { Box::from_raw(input.cast()) };
 }
 
+/// Sends the given output to subscribed dora nodes/operators.
+///
+/// The `id_ptr` and `id_len` fields must be the start pointer and length of an
+/// UTF8-encoded string. The ID string must correspond to one of the node's
+/// outputs specified in the dataflow YAML file.
+///
+/// The `data_ptr` and `data_len` fields must be the start pointer and length
+/// a byte array. The dora API sends this data as-is, without any processing.
+///
+/// ## Safety
+///
+/// - The `id_ptr` and `id_len` fields must be the start pointer and length of an
+///   UTF8-encoded string.
+/// - The `data_ptr` and `data_len` fields must be the start pointer and length
+///   a byte array.
 #[no_mangle]
 pub unsafe extern "C" fn dora_send_output(
     context: *mut (),
