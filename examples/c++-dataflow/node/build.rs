09866cbb2b0077bc0188b41a129b9c17493c8062
Philipp Oppermann <dev@phil-opp.com>
2022-08-11 14:30:52 +0200

    Create an example C++ node based on Rust node API and `cxx` crate

---
+++ examples/c++-dataflow/node/build.rs
@@ -0,0 +1,10 @@
+fn main() {
+    cxx_build::bridge("src/main.rs") // returns a cc::Build
+        .file("src/main.cc")
+        .flag_if_supported("-std=c++11")
+        .compile("cxx-example-dataflow-node");
+
+    println!("cargo:rerun-if-changed=src/main.rs");
+    println!("cargo:rerun-if-changed=src/main.cc");
+    println!("cargo:rerun-if-changed=src/main.h");
+}
