222b81f201ce20e1778f651ce072a521c0cceb4f
Philipp Oppermann <dev@phil-opp.com>
2022-08-11 18:13:49 +0200

    Add an example C++ operator based on the Rust API

---
+++ examples/c++-dataflow/operator-rust-api/src/operator.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "rust/cxx.h"
+#include <memory>
+
+class Operator
+{
+public:
+    Operator();
+    unsigned char counter;
+};
+
+std::unique_ptr<Operator> new_operator();
+
+struct OnInputResult;
+struct OutputSender;
+
+OnInputResult on_input(Operator &op, rust::Str, rust::Slice<const uint8_t>, OutputSender &output_sender);
