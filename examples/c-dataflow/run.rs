9c066aa11faa2814b7d9a1daa595c8f0a3d614ee
Philipp Oppermann <dev@phil-opp.com>
2022-08-19 12:05:22 +0200

    Link windows code with dynamic runtime

--- examples/c-dataflow/run.rs
+++ examples/c-dataflow/run.rs
@@ -60,6 +60,9 @@ async fn build_c_node(root: &Path, name: &str, out_name: &str) -> eyre::Result<(
     #[cfg(target_os = "windows")]
     {
         clang.arg("-lws2_32");
+        clang.arg("-Wl,-nodefaultlib:libcmt");
+        clang.arg("-D_DLL");
+        clang.arg("-lmsvcrt");
     }
     #[cfg(target_os = "macos")]
     {
