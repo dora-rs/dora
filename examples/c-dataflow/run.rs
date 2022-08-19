a14937f11bb1ac914a75a60cf1b2df2759a5112f
Philipp Oppermann <dev@phil-opp.com>
2022-08-19 13:52:53 +0200

    Link more libraries on Windows

--- examples/c-dataflow/run.rs
+++ examples/c-dataflow/run.rs
@@ -59,7 +59,12 @@ async fn build_c_node(root: &Path, name: &str, out_name: &str) -> eyre::Result<(
     }
     #[cfg(target_os = "windows")]
     {
+        clang.arg("-ladvapi32");
+        clang.arg("-luserenv");
+        clang.arg("-lkernel32");
         clang.arg("-lws2_32");
+        clang.arg("-lbcrypt");
+
         clang.arg("-Wl,-nodefaultlib:libcmt");
         clang.arg("-D_DLL");
         clang.arg("-lmsvcrt");
