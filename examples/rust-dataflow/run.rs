dd50d2ebd83a50a243fa9216b0d348f03ac23a3d
Philipp Oppermann <dev@phil-opp.com>
2022-09-29 14:11:24 +0200

    Add support for build command to nodes and operator definitions

--- examples/rust-dataflow/run.rs
+++ examples/rust-dataflow/run.rs
@@ -7,17 +7,28 @@ async fn main() -> eyre::Result<()> {
     std::env::set_current_dir(root.join(file!()).parent().unwrap())
         .wrap_err("failed to set working dir")?;
 
-    build_package("rust-dataflow-example-node").await?;
-    build_package("rust-dataflow-example-operator").await?;
-    build_package("rust-dataflow-example-sink").await?;
+    let dataflow = Path::new("dataflow.yml");
+    build_dataflow(dataflow).await?;
     build_package("dora-runtime").await?;
 
     dora_coordinator::run(dora_coordinator::Command::Run {
-        dataflow: Path::new("dataflow.yml").to_owned(),
+        dataflow: dataflow.to_owned(),
         runtime: Some(root.join("target").join("debug").join("dora-runtime")),
     })
     .await?;
 
+    Ok(())
+}
+
+async fn build_dataflow(dataflow: &Path) -> eyre::Result<()> {
+    let cargo = std::env::var("CARGO").unwrap();
+    let mut cmd = tokio::process::Command::new(&cargo);
+    cmd.arg("run");
+    cmd.arg("--package").arg("dora-cli");
+    cmd.arg("--").arg("build").arg(dataflow);
+    if !cmd.status().await?.success() {
+        bail!("failed to build dataflow");
+    };
     Ok(())
 }
 
