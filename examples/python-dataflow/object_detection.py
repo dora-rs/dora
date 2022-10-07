0985b7ceee1fd745f5e5d8339ea4cbfb160b348c
haixuanTao <tao.xavier@outlook.com>
2022-10-07 15:16:03 +0200

    Rename `input` to `dora_input` for python ex

--- examples/python-dataflow/object_detection.py
+++ examples/python-dataflow/object_detection.py
@@ -21,22 +21,21 @@ class Operator:
 
     def on_input(
         self,
-        input: dict,
+        dora_input: dict,
         send_output: Callable[[str, bytes], None],
     ) -> DoraStatus:
         """Handle image
 
         Args:
-            input_id (str): Id of the input declared in the yaml configuration
-            value (bytes): Bytes message of the input
+            dora_input (dict): Dict containing the "id", "data", and "metadata"
             send_output (Callable[[str, bytes]]): Function enabling sending output back to dora.
         """
 
-        frame = np.frombuffer(input["data"], dtype="uint8")
+        frame = np.frombuffer(dora_input["data"], dtype="uint8")
         frame = cv2.imdecode(frame, -1)
         frame = frame[:, :, ::-1]  # OpenCV image (BGR to RGB)
 
         results = self.model(frame)  # includes NMS
         arrays = np.array(results.xyxy[0].cpu()).tobytes()
-        send_output("bbox", arrays, input["metadata"])
+        send_output("bbox", arrays, dora_input["metadata"])
         return DoraStatus.CONTINUE
