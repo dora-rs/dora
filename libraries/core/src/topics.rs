f063af1df6e0abfc2bae7b00c8fcd01dfc17d0a5
Philipp Oppermann <dev@phil-opp.com>
2022-10-14 20:10:53 +0200

    Implement `stop` subcommand to send `stop` message to dataflow
    The message is not recognized by the nodes/operators yet. This will be done in a follow-up commit.

--- libraries/core/src/topics.rs
+++ libraries/core/src/topics.rs
@@ -1,9 +1,16 @@
 pub const ZENOH_CONTROL_QUERYABLE: &str = "dora_control/*";
 pub const ZENOH_CONTROL_START: &str = "dora_control/start";
+pub const ZENOH_CONTROL_STOP: &str = "dora_control/stop";
 pub const ZENOH_CONTROL_DESTROY: &str = "dora_control/destroy";
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub enum StartDataflowResult {
     Ok { uuid: String },
+    Error(String),
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum StopDataflowResult {
+    Ok,
     Error(String),
 }
