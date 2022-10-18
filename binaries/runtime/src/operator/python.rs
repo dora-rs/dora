3b89e61b36a4265f6ac3345ef153f75422179173
Philipp Oppermann <dev@phil-opp.com>
2022-10-18 16:47:30 +0200

    Fix opentelemetry access

--- binaries/runtime/src/operator/python.rs
+++ binaries/runtime/src/operator/python.rs
@@ -98,7 +98,7 @@ pub fn spawn(
                 use opentelemetry::trace::TraceContextExt;
                 use opentelemetry::{trace::Tracer, Context as OtelContext};
 
-                let cx = deserialize_context(&input.metadata.open_telemetry_context);
+                let cx = deserialize_context(&input.metadata.parameters.open_telemetry_context);
                 let span = tracer.start_with_context(format!("{}", input.id), &cx);
 
                 let child_cx = OtelContext::current_with_span(span);
