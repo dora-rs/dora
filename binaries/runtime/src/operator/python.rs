fc27e9d81f7bd57e571d67c614c5f152ef0f4310
Xavier Tao <tao.xavier@outlook.com>
2022-10-27 14:59:56 -0700

    Changing `drop_operator` to `__del__` in `python.rs`
    Using the default python destructor function for dora operator simplify
    the usability of dora as user does not have to learn as many custom
    methods.
    
    This also creates a symmetry with __init__ and  reduce the risk of
    errors if drop_operator is not present or misspelled.
    
    It also enables the usability of dora operator outside of dora ( for
    testing dor example ) and let it be naturally garbage collected.

--- binaries/runtime/src/operator/python.rs
+++ binaries/runtime/src/operator/python.rs
@@ -158,15 +158,8 @@ pub fn spawn(
         }
 
         Python::with_gil(|py| {
-            let operator = operator.as_ref(py);
-            if operator
-                .hasattr("drop_operator")
-                .wrap_err("failed to look for drop_operator")?
-            {
-                operator.call_method0("drop_operator")?;
-            }
-            Result::<_, eyre::Report>::Ok(())
-        })?;
+            drop(operator);
+        });
 
         Result::<_, eyre::Report>::Ok(())
     };
