db8453ae909025b89e19b78c85c0a2a66bc04060
Philipp Oppermann <dev@phil-opp.com>
2022-11-09 14:24:46 +0100

    Fix clippy warnings

--- binaries/cli/src/template/rust/mod.rs
+++ binaries/cli/src/template/rust/mod.rs
@@ -32,7 +32,7 @@ fn create_dataflow(name: String, path: Option<PathBuf>) -> Result<(), eyre::ErrR
 
     // create directories
     let root = path.as_deref().unwrap_or_else(|| Path::new(&name));
-    fs::create_dir(&root)
+    fs::create_dir(root)
         .with_context(|| format!("failed to create directory `{}`", root.display()))?;
 
     let dataflow_yml = DATAFLOW_YML.replace("___name___", &name);
@@ -75,7 +75,7 @@ fn create_operator(name: String, path: Option<PathBuf>) -> Result<(), eyre::ErrR
 
     // create directories
     let root = path.as_deref().unwrap_or_else(|| Path::new(&name));
-    fs::create_dir(&root)
+    fs::create_dir(root)
         .with_context(|| format!("failed to create directory `{}`", root.display()))?;
     let src = root.join("src");
     fs::create_dir(&src)
@@ -111,7 +111,7 @@ fn create_custom_node(name: String, path: Option<PathBuf>) -> Result<(), eyre::E
 
     // create directories
     let root = path.as_deref().unwrap_or_else(|| Path::new(&name));
-    fs::create_dir(&root)
+    fs::create_dir(root)
         .with_context(|| format!("failed to create directory `{}`", root.display()))?;
     let src = root.join("src");
     fs::create_dir(&src)
