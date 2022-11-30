c1eb8c29a9883f37579be10df870a4397da60c53
Philipp Oppermann <dev@phil-opp.com>
2022-11-30 21:16:20 +0100

    Update nodes of rust example to new API

--- examples/rust-dataflow/node/src/main.rs
+++ examples/rust-dataflow/node/src/main.rs
@@ -1,32 +1,36 @@
-use dora_node_api::{self, dora_core::config::DataId, DoraNode};
+use dora_node_api::{
+    self,
+    dora_core::{config::DataId, daemon_messages::NodeEvent},
+    DoraNode,
+};
 
 fn main() -> eyre::Result<()> {
     let output = DataId::from("random".to_owned());
 
-    let mut operator = DoraNode::init_from_env()?;
-
-    let inputs = operator.inputs()?;
+    let (mut node, events) = DoraNode::init_from_env()?;
 
     for _ in 0..20 {
-        let input = match inputs.recv() {
+        let event = match events.recv() {
             Ok(input) => input,
             Err(_) => break,
         };
 
-        match input.id.as_str() {
-            "tick" => {
-                let random: u64 = rand::random();
-                let data: &[u8] = &random.to_le_bytes();
-                operator.send_output(
-                    &output,
-                    input.metadata().parameters.clone(),
-                    data.len(),
-                    |out| {
+        match event {
+            NodeEvent::Stop => break,
+            NodeEvent::Input {
+                id,
+                metadata,
+                data: _,
+            } => match id.as_str() {
+                "tick" => {
+                    let random: u64 = rand::random();
+                    let data: &[u8] = &random.to_le_bytes();
+                    node.send_output(output.clone(), metadata.parameters, data.len(), |out| {
                         out.copy_from_slice(data);
-                    },
-                )?;
-            }
-            other => eprintln!("Ignoring unexpected input `{other}`"),
+                    })?;
+                }
+                other => eprintln!("Ignoring unexpected input `{other}`"),
+            },
         }
     }
 
