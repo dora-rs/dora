aa85b6ee4183a9381e299d6ca962878e883fb446
Philipp Oppermann <dev@phil-opp.com>
2023-01-02 17:21:21 +0100

    Increase benchmark messages sizes (up to 10000 4k pages)

--- examples/benchmark/node/src/main.rs
+++ examples/benchmark/node/src/main.rs
@@ -5,7 +5,20 @@ fn main() -> eyre::Result<()> {
     let output = DataId::from("random".to_owned());
 
     let (mut node, _events) = DoraNode::init_from_env()?;
-    for size in [0, 8, 64, 512, 2048, 4096, 4 * 4096, 10 * 4096] {
+    let sizes = [
+        0,
+        8,
+        64,
+        512,
+        2048,
+        4096,
+        4 * 4096,
+        10 * 4096,
+        100 * 4096,
+        1000 * 4096,
+        10000 * 4096,
+    ];
+    for size in sizes {
         for _ in 0..100 {
             let data: Vec<u8> = rand::thread_rng()
                 .sample_iter(rand::distributions::Standard)
