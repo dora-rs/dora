c6d864b20f2e75a25679a9d2bd66ad79c39791aa
Philipp Oppermann <dev@phil-opp.com>
2023-01-11 20:17:18 +0100

    Make shmem server/client typed and report event stream closure

--- libraries/core/src/shared_memory/mod.rs
+++ libraries/core/src/shared_memory/mod.rs
@@ -1,26 +1,28 @@
-use std::time::Duration;
-
 use self::channel::ShmemChannel;
 use eyre::{eyre, Context};
 use serde::{Deserialize, Serialize};
 use shared_memory::Shmem;
+use std::marker::PhantomData;
+use std::time::Duration;
 
 mod channel;
 
-pub struct ShmemServer {
+pub struct ShmemServer<T, U> {
     channel: ShmemChannel,
     reply_expected: bool,
+    phantom: PhantomData<(T, U)>,
 }
 
-impl ShmemServer {
+impl<T, U> ShmemServer<T, U> {
     pub unsafe fn new(memory: Shmem) -> eyre::Result<Self> {
         Ok(Self {
             channel: ShmemChannel::new_server(memory)?,
             reply_expected: false,
+            phantom: PhantomData,
         })
     }
 
-    pub fn listen<T>(&mut self) -> eyre::Result<Option<T>>
+    pub fn listen(&mut self) -> eyre::Result<Option<T>>
     where
         T: for<'a> Deserialize<'a> + std::fmt::Debug,
     {
@@ -33,9 +35,9 @@ impl ShmemServer {
         result
     }
 
-    pub fn send_reply<T>(&mut self, value: &T) -> eyre::Result<()>
+    pub fn send_reply(&mut self, value: &U) -> eyre::Result<()>
     where
-        T: Serialize + std::fmt::Debug,
+        U: Serialize + std::fmt::Debug,
     {
         assert!(self.reply_expected);
         self.channel.send(value)?;
@@ -44,20 +46,22 @@ impl ShmemServer {
     }
 }
 
-pub struct ShmemClient {
+pub struct ShmemClient<T, U> {
     channel: ShmemChannel,
     timeout: Option<Duration>,
+    phantom: PhantomData<(T, U)>,
 }
 
-impl ShmemClient {
+impl<T, U> ShmemClient<T, U> {
     pub unsafe fn new(memory: Shmem, timeout: Option<Duration>) -> eyre::Result<Self> {
         Ok(Self {
             channel: ShmemChannel::new_client(memory)?,
             timeout,
+            phantom: PhantomData,
         })
     }
 
-    pub fn request<T, U>(&mut self, value: &T) -> eyre::Result<U>
+    pub fn request(&mut self, value: &T) -> eyre::Result<U>
     where
         T: Serialize + std::fmt::Debug,
         U: for<'a> Deserialize<'a> + std::fmt::Debug,
