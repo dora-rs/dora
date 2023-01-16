5bd0eda5d90be334b25c73c7af7647f70eaccd0f
haixuanTao <tao.xavier@outlook.com>
2023-01-16 16:20:46 +0100

    Package `DoraStatus` into dora python package.
    To prepare for future feature, this commit initiate a `__init__.py` file
    that makes the dora python node api, a mixed python/rust project.
    
    Any python function or variable included in the `__init__.py` will be
    distributed along rust function declared in `src/lib.rs`.
    
    https://github.com/dora-rs/dora/issues/163 has shown the limitation on
    depending too much on `pyo3`.

---
+++ apis/python/node/dora/__init__.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+from .dora import *
+
+
+class DoraStatus(Enum):
+    """Dora status to indicate if operator `on_input` loop
+     should be break
+
+    Args:
+        Enum (u8): Status signaling to dora operator to
+        stop or continue the operator.
+    """
+
+    CONTINUE = 0
+    STOP = 1
+    STOP_ALL = 2
