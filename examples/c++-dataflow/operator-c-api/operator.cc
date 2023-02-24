f54ac08413b27f1e9072a7308c6c079e4cd51bdf
Philipp Oppermann <dev@phil-opp.com>
2023-02-24 11:51:55 +0100

    Add examples for C++ nodes and operators based on C-API again

--- examples/c++-dataflow/operator-c-api/operator.cc
+++ examples/c++-dataflow/operator-c-api/operator.cc
@@ -30,44 +30,55 @@ extern "C" DoraResult_t dora_drop_operator(void *operator_context)
     return {};
 }
 
-extern "C" OnInputResult_t dora_on_input(
-    const Input_t *input,
+extern "C" OnEventResult_t dora_on_event(
+    const RawEvent_t *event,
     const SendOutput_t *send_output,
     void *operator_context)
 {
-
-    std::string id((char *)input->id.ptr, input->id.len);
-
-    std::vector<unsigned char> data;
-    for (size_t i = 0; i < input->data.len; i++)
+    if (event->input != NULL)
     {
-        data.push_back(*(input->data.ptr + i));
+        // input event
+        Input_t *input = event->input;
+        std::string id((char *)input->id.ptr, input->id.len);
+
+        std::vector<unsigned char> data;
+        for (size_t i = 0; i < input->data.len; i++)
+        {
+            data.push_back(*(input->data.ptr + i));
+        }
+
+        std::cout
+            << "C++ Operator (C-API) received input `" << id << "` with data: [";
+        for (unsigned char &v : data)
+        {
+            std::cout << (unsigned int)v << ", ";
+        }
+        std::cout << "]" << std::endl;
+
+        const char *out_id = "half-status";
+        char *out_id_heap = strdup(out_id);
+
+        size_t out_data_len = 1;
+        uint8_t *out_data_heap = (uint8_t *)malloc(out_data_len);
+        *out_data_heap = data[0] / 2;
+
+        Output_t output = {.id = {
+                               .ptr = (uint8_t *)out_id_heap,
+                               .len = strlen(out_id_heap),
+                               .cap = strlen(out_id_heap) + 1,
+                           },
+                           .data = {.ptr = out_data_heap, .len = out_data_len, .cap = out_data_len}};
+
+        DoraResult_t send_result = (send_output->send_output.call)(send_output->send_output.env_ptr, output);
+
+        OnEventResult_t result = {.result = send_result, .status = DORA_STATUS_CONTINUE};
+        return result;
+    }
+    if (event->stop)
+    {
+        printf("C operator received stop event\n");
     }
 
-    std::cout
-        << "C++ Operator (C-API) received input `" << id << "` with data: [";
-    for (unsigned char &v : data)
-    {
-        std::cout << (unsigned int)v << ", ";
-    }
-    std::cout << "]" << std::endl;
-
-    const char *out_id = "half-status";
-    char *out_id_heap = strdup(out_id);
-
-    size_t out_data_len = 1;
-    uint8_t *out_data_heap = (uint8_t *)malloc(out_data_len);
-    *out_data_heap = data[0] / 2;
-
-    Output_t output = {.id = {
-                           .ptr = (uint8_t *)out_id_heap,
-                           .len = strlen(out_id_heap),
-                           .cap = strlen(out_id_heap) + 1,
-                       },
-                       .data = {.ptr = out_data_heap, .len = out_data_len, .cap = out_data_len}};
-
-    DoraResult_t send_result = (send_output->send_output.call)(send_output->send_output.env_ptr, output);
-
-    OnInputResult_t result = {.result = send_result, .status = DORA_STATUS_CONTINUE};
+    OnEventResult_t result = {.status = DORA_STATUS_CONTINUE};
     return result;
 }
