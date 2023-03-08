5580ec3d0829ce2a69725e4f7a224ba56ca0f64b
Philipp Oppermann <dev@phil-opp.com>
2023-03-08 10:19:47 +0100

    Refactor: Split Rust node API into smaller submodules

--- apis/rust/node/src/lib.rs
+++ apis/rust/node/src/lib.rs
@@ -1,8 +1,10 @@
-pub use daemon_connection::{Event, EventStream};
+pub use daemon_connection::EventStream;
 pub use dora_core;
 pub use dora_core::message::{uhlc, Metadata, MetadataParameters};
+pub use event::{Event, MappedInputData};
 pub use flume::Receiver;
 pub use node::DoraNode;
 
 mod daemon_connection;
+mod event;
 mod node;
