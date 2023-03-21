c112b6bd59bf32dd3416d388a5c7010dc655cb8b
haixuanTao <tao.xavier@outlook.com>
2023-03-21 19:06:54 +0800

    make header file const instead of static

--- apis/c/operator/src/lib.rs
+++ apis/c/operator/src/lib.rs
@@ -1,2 +1,2 @@
-pub static HEADER_OPERATOR_API: &str = include_str!("../operator_api.h");
-pub static HEADER_OPERATOR_TYPES: &str = include_str!("../operator_types.h");
+pub const HEADER_OPERATOR_API: &str = include_str!("../operator_api.h");
+pub const HEADER_OPERATOR_TYPES: &str = include_str!("../operator_types.h");
