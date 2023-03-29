d3b612faf2f3cf02d477e600f577780c5ee56cb1
haixuanTao <tao.xavier@outlook.com>
2023-03-29 18:18:48 +0800

    Use opentelemetry if env variable `DORA_JAEGER_TRACING` is set

--- libraries/extensions/telemetry/tracing/src/lib.rs
+++ libraries/extensions/telemetry/tracing/src/lib.rs
@@ -6,7 +6,8 @@
 use eyre::Context as EyreContext;
 use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, EnvFilter, Layer};
 
-#[cfg(feature = "telemetry")]
+use eyre::ContextCompat;
+use tracing_subscriber::Registry;
 pub mod telemetry;
 
 pub fn set_up_tracing(name: &str) -> eyre::Result<()> {
@@ -16,43 +17,21 @@ pub fn set_up_tracing(name: &str) -> eyre::Result<()> {
         .pretty()
         .with_filter(filter);
 
-    #[cfg(feature = "telemetry")]
-    let subscriber = {
-        use eyre::ContextCompat;
-        use opentelemetry::sdk::export::trace::stdout;
-        use tracing::metadata::LevelFilter;
-        use tracing_subscriber::Registry;
-
-        let filter = EnvFilter::from_default_env();
-        let telemetry = match std::env::var_os("DORA_JAEGER_TRACING") {
-            Some(endpoint) => {
-                let endpoint = endpoint
-                    .to_str()
-                    .wrap_err("Could not parse env variable: DORA_JAEGER_TRACING")?;
-                let tracer = crate::telemetry::init_jaeger_tracing(name, endpoint)
-                    .wrap_err("Could not instantiate tracing")?;
-                tracing_opentelemetry::layer()
-                    .with_tracer(tracer)
-                    .with_filter(filter.add_directive(LevelFilter::TRACE.into()))
-            }
-            None => {
-                let tracer = stdout::new_pipeline()
-                    .with_pretty_print(true)
-                    .install_simple();
-                tracing_opentelemetry::layer()
-                    .with_tracer(tracer)
-                    .with_filter(filter)
-            }
-        };
-        // Create a tracing layer with the configured tracer
-        Registry::default().with(stdout_log).with(telemetry)
-    };
-    #[cfg(not(feature = "telemetry"))]
-    let subscriber = tracing_subscriber::Registry::default().with(stdout_log);
-
-    // Use the tracing subscriber `Registry`, or any other subscriber
-    // that impls `LookupSpan`
-    tracing::subscriber::set_global_default(subscriber).context(format!(
-        "failed to set tracing global subscriber for {name}"
-    ))
+    let registry = Registry::default().with(stdout_log);
+    if let Some(endpoint) = std::env::var_os("DORA_JAEGER_TRACING") {
+        let endpoint = endpoint
+            .to_str()
+            .wrap_err("Could not parse env variable: DORA_JAEGER_TRACING")?;
+        let tracer = crate::telemetry::init_jaeger_tracing(name, endpoint)
+            .wrap_err("Could not instantiate tracing")?;
+        let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+        let subscriber = registry.with(telemetry);
+        tracing::subscriber::set_global_default(subscriber).context(format!(
+            "failed to set tracing global subscriber for {name}"
+        ))
+    } else {
+        tracing::subscriber::set_global_default(registry).context(format!(
+            "failed to set tracing global subscriber for {name}"
+        ))
+    }
 }
