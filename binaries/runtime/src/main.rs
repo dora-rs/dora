79c1037399992394715bae55c07b649256a79d15
haixuanTao <tao.xavier@outlook.com>
2023-03-29 14:19:25 +0800

    Add version print in runtime

--- binaries/runtime/src/main.rs
+++ binaries/runtime/src/main.rs
@@ -1,3 +1,11 @@
+use clap::Parser;
+
+/// Show author, version and about.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {}
+
 fn main() -> Result<(), eyre::Report> {
+    Args::parse();
     dora_runtime::main()
 }
