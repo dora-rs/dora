b3ce69b86cae12bfd4dafde88b4f85fa9e94c1da
Philipp Oppermann <dev@phil-opp.com>
2023-06-23 20:43:33 +0200

    Delete other rclrust crates, we only need the message generator

--- rclrust-msg/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-env-changed=AMENT_PREFIX_PATH");
-
-    let ament_prefix_paths =
-        std::env::var("AMENT_PREFIX_PATH").expect("$AMENT_PREFIX_PATH is supposed to be set.");
-    for ament_prefix_path in ament_prefix_paths.split(':') {
-        println!("cargo:rustc-link-search=native={}/lib", ament_prefix_path);
-    }
-}
--- rclrust-msg/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![warn(rust_2018_idioms, elided_lifetimes_in_paths)]
-#![allow(clippy::all)]
-
-pub mod _core;
-
-rclrust_msg_gen::msg_include_all!();
