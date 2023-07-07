9b01f03787f4dbeb6dce066e975ce47110d4b1b4
Philipp Oppermann <dev@phil-opp.com>
2023-07-07 13:52:50 +0200

    Parse ROS2 messages on context generation and start implementing TypeInfo conversion

--- python/test.py
+++ python/test.py
@@ -8,7 +8,8 @@ topic_qos = Ros2QosPolicies()
 topic_qos.reliability = Ros2Reliability.Reliable
 topic_qos.reliability_max_blocking_time = 0.1
 
-turtle_pose_topic = node.create_topic("/turtle1/pose", "turtlesim::msg::dds_::Pose_", topic_qos)
+turtle_pose_topic = node.create_topic("/turtle1/pose", "turtlesim::Pose", topic_qos)
+
 pose_reader = node.create_subscription(turtle_pose_topic, topic_qos)
 
 for i in range(500):
