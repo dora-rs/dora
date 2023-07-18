d55df9a3261b670fab5ab8530ac7f9f84996c54a
haixuanTao <tao.xavier@outlook.com>
2023-07-18 17:45:19 +0200

    Adding documentation for the send method
    This PR/commit adds documentation for the send_output method.
    
    Follow up discussion: https://github.com/orgs/dora-rs/discussions/317#discussioncomment-6475816

--- binaries/cli/src/template/python/operator/operator-template.py
+++ binaries/cli/src/template/python/operator/operator-template.py
@@ -15,7 +15,7 @@ class Operator:
     def on_event(
         self,
         dora_event: dict,
-        send_output: Callable[[str, bytes], None],
+        send_output: Callable[[str, bytes, dict], None],
     ) -> DoraStatus:
         if dora_event["type"] == "INPUT":
             return self.on_input(dora_event, send_output)
@@ -24,13 +24,18 @@ class Operator:
     def on_input(
         self,
         dora_input: dict,
-        send_output: Callable[[str, bytes], None],
+        send_output: Callable[[str, bytes, dict], None],
     ):
         """
 
         Args:
             dora_input (dict): Input dict containing an `id`, `data` and `metadata`.
-            send_output (Callable[[str, bytes], None]): Send output to the dataflow
+            send_output Callable[[str, bytes | pa.UInt8Array, dict], None]: 
+                Function for sending output to the dataflow:
+                - First argument is the `output_id`
+                - Second argument is the data as either bytes or `pa.UInt8Array` 
+                - Third argument is dora metadata dict
+                e.g.: `send_output("bbox", pa.array([100], type=pa.uint8()), dora_event["metadata"])`
 
         Returns:
             DoraStatus:
