d02f473130e003efec3338f559c5eaae0f2066a1
Philipp Oppermann <dev@phil-opp.com>
2023-07-21 13:02:11 +0200

    Remove local copy of `ros2-client` again

--- ros2-client/src/interfaces/mod.rs
+++
@@ -1,9 +0,0 @@
-mod basic_types_interface;
-mod get_parameters_interface;
-mod list_parameters_interface;
-//mod marker_interface;
-
-pub use basic_types_interface::*;
-pub use get_parameters_interface::*;
-pub use list_parameters_interface::*;
-//pub use marker_interface::*;
