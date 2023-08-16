807cd6153cec4b477261a00cced9a4b6062a98ba
Philipp Oppermann <dev@phil-opp.com>
2023-08-16 15:33:03 +0200

    Fix: Build rust-ros2-dataflow-example-node when running example

--- examples/rust-ros2-dataflow/run.rs
+++ examples/rust-ros2-dataflow/run.rs
@@ -7,9 +7,24 @@ async fn main() -> eyre::Result<()> {
     std::env::set_current_dir(root.join(file!()).parent().unwrap())
         .wrap_err("failed to set working dir")?;
 
-    let dataflow = Path::new("dataflow.yml");
+    build_package("rust-ros2-dataflow-example-node")
+        .await
+        .wrap_err("failed to build rust-ros2-dataflow-example-node")?;
 
+    let dataflow = Path::new("dataflow.yml");
     dora_daemon::Daemon::run_dataflow(dataflow).await?;
 
+    Ok(())
+}
+
+async fn build_package(name: &str) -> eyre::Result<()> {
+    let cargo = std::env::var("CARGO").unwrap();
+    let mut cmd = tokio::process::Command::new(&cargo);
+    cmd.arg("build");
+    cmd.arg("--package").arg(name);
+    cmd.arg("--features").arg("ros2");
+    if !cmd.status().await?.success() {
+        eyre::bail!("failed to build dataflow");
+    };
     Ok(())
 }
