3be0842e213333abd20ba7fa02b5251fc2e7699c
Philipp Oppermann <dev@phil-opp.com>
2023-08-22 16:57:02 +0200

    Add new MergeExternalSend trait for usage in Python
    The only difference betwenn the two traits is the additional Send bound. We should unify this again using the trait-level impl trait feature once it's stable.

--- apis/rust/node/src/event_stream/merged.rs
+++ apis/rust/node/src/event_stream/merged.rs
@@ -20,6 +20,7 @@ impl<A> Either<A, A> {
     }
 }
 
+// TODO: use impl trait return type once stable
 pub trait MergeExternal<'a, E> {
     type Item;
 
@@ -27,6 +28,15 @@ pub trait MergeExternal<'a, E> {
         self,
         external_events: impl Stream<Item = E> + Unpin + 'a,
     ) -> Box<dyn Stream<Item = Self::Item> + Unpin + 'a>;
+}
+
+pub trait MergeExternalSend<'a, E> {
+    type Item;
+
+    fn merge_external_send(
+        self,
+        external_events: impl Stream<Item = E> + Unpin + Send + 'a,
+    ) -> Box<dyn Stream<Item = Self::Item> + Unpin + Send + 'a>;
 }
 
 impl<'a, E> MergeExternal<'a, E> for super::EventStream
@@ -45,6 +55,22 @@ where
     }
 }
 
+impl<'a, E> MergeExternalSend<'a, E> for super::EventStream
+where
+    E: 'static,
+{
+    type Item = MergedEvent<E>;
+
+    fn merge_external_send(
+        self,
+        external_events: impl Stream<Item = E> + Unpin + Send + 'a,
+    ) -> Box<dyn Stream<Item = Self::Item> + Unpin + Send + 'a> {
+        let dora = self.map(MergedEvent::Dora);
+        let external = external_events.map(MergedEvent::External);
+        Box::new((dora, external).merge())
+    }
+}
+
 impl<'a, E, F, S> MergeExternal<'a, F> for S
 where
     S: Stream<Item = MergedEvent<E>> + Unpin + 'a,
@@ -57,6 +83,27 @@ where
         self,
         external_events: impl Stream<Item = F> + Unpin + 'a,
     ) -> Box<dyn Stream<Item = Self::Item> + Unpin + 'a> {
+        let first = self.map(|e| match e {
+            MergedEvent::Dora(d) => MergedEvent::Dora(d),
+            MergedEvent::External(e) => MergedEvent::External(Either::First(e)),
+        });
+        let second = external_events.map(|e| MergedEvent::External(Either::Second(e)));
+        Box::new((first, second).merge())
+    }
+}
+
+impl<'a, E, F, S> MergeExternalSend<'a, F> for S
+where
+    S: Stream<Item = MergedEvent<E>> + Unpin + Send + 'a,
+    E: 'a,
+    F: 'a,
+{
+    type Item = MergedEvent<Either<E, F>>;
+
+    fn merge_external_send(
+        self,
+        external_events: impl Stream<Item = F> + Unpin + Send + 'a,
+    ) -> Box<dyn Stream<Item = Self::Item> + Unpin + Send + 'a> {
         let first = self.map(|e| match e {
             MergedEvent::Dora(d) => MergedEvent::Dora(d),
             MergedEvent::External(e) => MergedEvent::External(Either::First(e)),
