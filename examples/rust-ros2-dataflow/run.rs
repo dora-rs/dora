f90cf16381d084dabe1212bb59a60cc740088a5a
Philipp Oppermann <dev@phil-opp.com>
2023-08-22 16:59:16 +0200

    Update dependency and add tracing for rust example

--- examples/rust-ros2-dataflow/run.rs
+++ examples/rust-ros2-dataflow/run.rs
@@ -1,8 +1,15 @@
 use eyre::WrapErr;
 use std::path::Path;
+use tracing_subscriber::{
+    filter::{FilterExt, LevelFilter},
+    prelude::*,
+    EnvFilter, Registry,
+};
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
+    set_up_tracing()?;
+
     let root = Path::new(env!("CARGO_MANIFEST_DIR"));
     std::env::set_current_dir(root.join(file!()).parent().unwrap())
         .wrap_err("failed to set working dir")?;
@@ -27,4 +34,17 @@ async fn build_package(name: &str) -> eyre::Result<()> {
         eyre::bail!("failed to build dataflow");
     };
     Ok(())
+}
+
+pub fn set_up_tracing() -> eyre::Result<()> {
+    // Filter log using `RUST_LOG`. More useful for CLI.
+    let filter = EnvFilter::from_default_env().or(LevelFilter::DEBUG);
+    let stdout_log = tracing_subscriber::fmt::layer()
+        .pretty()
+        .with_filter(filter);
+
+    let registry = Registry::default().with(stdout_log);
+
+    tracing::subscriber::set_global_default(registry)
+        .context("failed to set tracing global subscriber")
 }
