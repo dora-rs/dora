b9e5e533e0486a2cc6ce03bdc84ea7cf1361dec5
Philipp Oppermann <dev@phil-opp.com>
2024-01-04 18:27:37 +0100

    Redesign ROS2 message struct generator to support `cxx_bridge`

--- libraries/extensions/ros2-bridge/src/lib.rs
+++ libraries/extensions/ros2-bridge/src/lib.rs
@@ -1,7 +1,10 @@
 pub use ros2_client;
 pub use rustdds;
 
-#[cfg(feature = "generate-messages")]
+#[cfg(all(feature = "generate-messages", feature = "cxx-bridge"))]
+dora_ros2_bridge_msg_gen_macro::msg_include_all!(cxx_bridge = true);
+
+#[cfg(all(feature = "generate-messages", not(feature = "cxx-bridge")))]
 dora_ros2_bridge_msg_gen_macro::msg_include_all!();
 
 pub mod _core;
