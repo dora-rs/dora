3e8f154903cf210f1af1ab96557e163ecbab5ed3
Philipp Oppermann <dev@phil-opp.com>
2024-01-05 14:33:02 +0100

    Replace proc macro by build script to generate C++ bindings
    The `cxx_build` crate does not support proc macro output (see https://github.com/dtolnay/cxx/issues/808), so we have to generate a standard Rust source file instead.

---
+++ libraries/extensions/ros2-bridge/build.rs
@@ -0,0 +1,42 @@
+use rust_format::Formatter;
+use std::path::PathBuf;
+
+fn main() {
+    let create_cxx_bridge = cfg!(feature = "cxx-bridge");
+    let paths = ament_prefix_paths();
+    let generated = dora_ros2_bridge_msg_gen::gen(paths.as_slice(), create_cxx_bridge);
+    let generated_string = rust_format::PrettyPlease::default()
+        .format_tokens(generated)
+        .unwrap();
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let target_file = out_dir.join("generated.rs");
+    std::fs::write(&target_file, generated_string).unwrap();
+    println!("cargo:rustc-env=GENERATED_PATH={}", target_file.display());
+
+    #[cfg(feature = "cxx-bridge")]
+    let _build = cxx_build::bridge(&target_file);
+}
+
+fn ament_prefix_paths() -> Vec<PathBuf> {
+    let ament_prefix_path: String = match std::env::var("AMENT_PREFIX_PATH") {
+        Ok(path) => path,
+        Err(std::env::VarError::NotPresent) => {
+            println!("cargo:warning='AMENT_PREFIX_PATH not set'");
+            String::new()
+        }
+        Err(std::env::VarError::NotUnicode(s)) => {
+            panic!(
+                "AMENT_PREFIX_PATH is not valid unicode: `{}`",
+                s.to_string_lossy()
+            );
+        }
+    };
+    println!("cargo:rerun-if-env-changed=AMENT_PREFIX_PATH");
+
+    let paths: Vec<_> = ament_prefix_path.split(':').map(PathBuf::from).collect();
+    for path in &paths {
+        println!("cargo:rerun-if-changed={}", path.display());
+    }
+
+    paths
+}
