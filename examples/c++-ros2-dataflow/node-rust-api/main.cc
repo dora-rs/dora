4237e78ea830075301eac7a374c993891752a16a
Philipp Oppermann <dev@phil-opp.com>
2024-01-12 15:53:47 +0100

    Update c++ ros2 example to trigger random movements

--- examples/c++-ros2-dataflow/node-rust-api/main.cc
+++ examples/c++-ros2-dataflow/node-rust-api/main.cc
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
 
 int main()
 {
@@ -19,18 +20,15 @@ int main()
     auto vel_topic = node->create_topic_geometry_msgs_Twist("/turtle1", "cmd_vel", qos);
     auto vel_publisher = node->create_publisher(vel_topic, qos);
 
-    geometry_msgs::Twist twist = {
-        .linear = {.x = 1, .y = 0, .z = 0},
-        .angular = {.x = 0, .y = 0, .z = 0.5}};
-
-    unsigned char counter = 0;
+    std::random_device dev;
+    std::default_random_engine gen(dev());
+    std::uniform_real_distribution<> dist(0., 1.);
 
     auto dora_node = init_dora_node();
+    auto received_ticks = 0;
 
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 1000; i++)
     {
-        vel_publisher->publish(twist);
-
         auto event = next_event(dora_node.events);
         auto ty = event_type(event);
 
@@ -41,24 +39,23 @@ int main()
         else if (ty == DoraEventType::Input)
         {
             auto input = event_as_input(std::move(event));
+            received_ticks += 1;
 
-            counter += 1;
+            std::cout << "Received input " << std::string(input.id) << std::endl;
 
-            std::cout << "Received input " << std::string(input.id) << " (counter: " << (unsigned int)counter << ")" << std::endl;
-
-            std::vector<unsigned char> out_vec{counter};
-            rust::Slice<const uint8_t> out_slice{out_vec.data(), out_vec.size()};
-            auto result = send_output(dora_node.send_output, "counter", out_slice);
-            auto error = std::string(result.error);
-            if (!error.empty())
-            {
-                std::cerr << "Error: " << error << std::endl;
-                return -1;
-            }
+            geometry_msgs::Twist twist = {
+                .linear = {.x = dist(gen) + 1, .y = 0, .z = 0},
+                .angular = {.x = 0, .y = 0, .z = (dist(gen) - 0.5) * 5.0}};
+            vel_publisher->publish(twist);
         }
         else
         {
             std::cerr << "Unknown event type " << static_cast<int>(ty) << std::endl;
+        }
+
+        if (received_ticks > 20)
+        {
+            break;
         }
     }
 
