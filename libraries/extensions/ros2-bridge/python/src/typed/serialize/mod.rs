1addfa385a4815a32e817423f99a933f403a4fc1
Philipp Oppermann <dev@phil-opp.com>
2024-01-25 16:22:45 +0100

    Add more context to error messages when serializing nested message

--- libraries/extensions/ros2-bridge/python/src/typed/serialize/mod.rs
+++ libraries/extensions/ros2-bridge/python/src/typed/serialize/mod.rs
@@ -1,10 +1,14 @@
 use std::{borrow::Cow, collections::HashMap, fmt::Display};
 
-use arrow::array::{Array, ArrayRef, AsArray};
+use arrow::{
+    array::{Array, ArrayRef, AsArray},
+    error,
+};
 use dora_ros2_bridge_msg_gen::types::{
     primitives::{GenericString, NestableType},
     MemberType,
 };
+use eyre::Context;
 use serde::ser::SerializeTupleStruct;
 
 use super::{TypeInfo, DUMMY_STRUCT_NAME};
@@ -42,8 +46,8 @@ impl serde::Serialize for TypedValue<'_> {
 
         let input = self.value.as_struct_opt().ok_or_else(|| {
             error(format!(
-                "expected struct array for message: {}, with following format: {:#?} \n But, got value: {:#?}",
-               self.type_info.message_name, message, self.value
+                "expected struct array for message: {}, with following format: {:#?} \n But, got type: {:#?}",
+               self.type_info.message_name, message, self.value.data_type()
             ))
         })?;
         for column_name in input.column_names() {
@@ -73,95 +77,122 @@ impl serde::Serialize for TypedValue<'_> {
                         &self.type_info.package_name,
                         &self.type_info.messages,
                     )
-                    .map_err(error)?;
+                    .with_context(|| {
+                        format!(
+                            "failed to calculate default value for field {}.{}",
+                            message.name, field.name
+                        )
+                    })
+                    .map_err(|e| error(format!("{e:?}")))?;
                     Cow::Owned(arrow::array::make_array(default))
                 }
             };
 
-            match &field.r#type {
-                MemberType::NestableType(t) => match t {
-                    NestableType::BasicType(t) => {
-                        s.serialize_field(&primitive::SerializeWrapper {
-                            t,
-                            column: column.as_ref(),
-                        })?;
-                    }
-                    NestableType::NamedType(name) => {
-                        let referenced_value = &TypedValue {
-                            value: column.as_ref(),
-                            type_info: &TypeInfo {
-                                package_name: Cow::Borrowed(&self.type_info.package_name),
-                                message_name: Cow::Borrowed(&name.0),
-                                messages: self.type_info.messages.clone(),
-                            },
-                        };
-                        s.serialize_field(&referenced_value)?;
-                    }
-                    NestableType::NamespacedType(reference) => {
-                        if reference.namespace != "msg" {
-                            return Err(error(format!(
-                                "struct field {} references non-message type {reference:?}",
-                                field.name
-                            )));
-                        }
-
-                        let referenced_value: &TypedValue<'_> = &TypedValue {
-                            value: column.as_ref(),
-                            type_info: &TypeInfo {
-                                package_name: Cow::Borrowed(&reference.package),
-                                message_name: Cow::Borrowed(&reference.name),
-                                messages: self.type_info.messages.clone(),
-                            },
-                        };
-                        s.serialize_field(&referenced_value)?;
-                    }
-                    NestableType::GenericString(t) => match t {
-                        GenericString::String | GenericString::BoundedString(_) => {
-                            let string = if let Some(string_array) = column.as_string_opt::<i32>() {
-                                // should match the length of the outer struct array
-                                assert_eq!(string_array.len(), 1);
-                                string_array.value(0)
-                            } else {
-                                // try again with large offset type
-                                let string_array = column
-                                    .as_string_opt::<i64>()
-                                    .ok_or_else(|| error("expected string array"))?;
-                                // should match the length of the outer struct array
-                                assert_eq!(string_array.len(), 1);
-                                string_array.value(0)
-                            };
-                            s.serialize_field(string)?;
-                        }
-                        GenericString::WString => todo!("serializing WString types"),
-                        GenericString::BoundedWString(_) => {
-                            todo!("serializing BoundedWString types")
-                        }
-                    },
-                },
-                dora_ros2_bridge_msg_gen::types::MemberType::Array(a) => {
-                    s.serialize_field(&array::ArraySerializeWrapper {
-                        array_info: a,
-                        column: column.as_ref(),
-                        type_info: self.type_info,
-                    })?;
-                }
-                dora_ros2_bridge_msg_gen::types::MemberType::Sequence(v) => {
-                    s.serialize_field(&sequence::SequenceSerializeWrapper {
-                        item_type: &v.value_type,
-                        column: column.as_ref(),
-                        type_info: self.type_info,
-                    })?;
-                }
-                dora_ros2_bridge_msg_gen::types::MemberType::BoundedSequence(v) => {
-                    s.serialize_field(&sequence::SequenceSerializeWrapper {
-                        item_type: &v.value_type,
-                        column: column.as_ref(),
-                        type_info: self.type_info,
-                    })?;
-                }
-            }
+            self.serialize_field::<S>(field, column, &mut s)
+                .map_err(|e| {
+                    error(format!(
+                        "failed to serialize field {}.{}: {e}",
+                        message.name, field.name
+                    ))
+                })?;
         }
         s.end()
+    }
+}
+
+impl<'a> TypedValue<'a> {
+    fn serialize_field<S>(
+        &self,
+        field: &dora_ros2_bridge_msg_gen::types::Member,
+        column: Cow<'_, std::sync::Arc<dyn Array>>,
+        s: &mut S::SerializeTupleStruct,
+    ) -> Result<(), S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match &field.r#type {
+            MemberType::NestableType(t) => match t {
+                NestableType::BasicType(t) => {
+                    s.serialize_field(&primitive::SerializeWrapper {
+                        t,
+                        column: column.as_ref(),
+                    })?;
+                }
+                NestableType::NamedType(name) => {
+                    let referenced_value = &TypedValue {
+                        value: column.as_ref(),
+                        type_info: &TypeInfo {
+                            package_name: Cow::Borrowed(&self.type_info.package_name),
+                            message_name: Cow::Borrowed(&name.0),
+                            messages: self.type_info.messages.clone(),
+                        },
+                    };
+                    s.serialize_field(&referenced_value)?;
+                }
+                NestableType::NamespacedType(reference) => {
+                    if reference.namespace != "msg" {
+                        return Err(error(format!(
+                            "struct field {} references non-message type {reference:?}",
+                            field.name
+                        )));
+                    }
+
+                    let referenced_value: &TypedValue<'_> = &TypedValue {
+                        value: column.as_ref(),
+                        type_info: &TypeInfo {
+                            package_name: Cow::Borrowed(&reference.package),
+                            message_name: Cow::Borrowed(&reference.name),
+                            messages: self.type_info.messages.clone(),
+                        },
+                    };
+                    s.serialize_field(&referenced_value)?;
+                }
+                NestableType::GenericString(t) => match t {
+                    GenericString::String | GenericString::BoundedString(_) => {
+                        let string = if let Some(string_array) = column.as_string_opt::<i32>() {
+                            // should match the length of the outer struct array
+                            assert_eq!(string_array.len(), 1);
+                            string_array.value(0)
+                        } else {
+                            // try again with large offset type
+                            let string_array = column
+                                .as_string_opt::<i64>()
+                                .ok_or_else(|| error("expected string array"))?;
+                            // should match the length of the outer struct array
+                            assert_eq!(string_array.len(), 1);
+                            string_array.value(0)
+                        };
+                        s.serialize_field(string)?;
+                    }
+                    GenericString::WString => todo!("serializing WString types"),
+                    GenericString::BoundedWString(_) => {
+                        todo!("serializing BoundedWString types")
+                    }
+                },
+            },
+            dora_ros2_bridge_msg_gen::types::MemberType::Array(a) => {
+                s.serialize_field(&array::ArraySerializeWrapper {
+                    array_info: a,
+                    column: column.as_ref(),
+                    type_info: self.type_info,
+                })?;
+            }
+            dora_ros2_bridge_msg_gen::types::MemberType::Sequence(v) => {
+                s.serialize_field(&sequence::SequenceSerializeWrapper {
+                    item_type: &v.value_type,
+                    column: column.as_ref(),
+                    type_info: self.type_info,
+                })?;
+            }
+            dora_ros2_bridge_msg_gen::types::MemberType::BoundedSequence(v) => {
+                s.serialize_field(&sequence::SequenceSerializeWrapper {
+                    item_type: &v.value_type,
+                    column: column.as_ref(),
+                    type_info: self.type_info,
+                })?;
+            }
+        }
+        Ok(())
     }
 }
 
