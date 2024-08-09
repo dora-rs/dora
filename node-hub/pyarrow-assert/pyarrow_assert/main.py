aea441b468f55dfd7e90573087241d5f6f28857a
haixuanTao <tao.xavier@outlook.com>
2024-08-09 12:36:55 +0200

    Adding `pyarrow-sender` and `terminal-print` in the node-hub

---
+++ node-hub/pyarrow-assert/pyarrow_assert/main.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+
+import numpy as np
+import pyarrow as pa
+
+from dora import Node
+
+RUNNER_CI = True if os.getenv("CI") == "true" else False
+
+
+def main():
+
+    # Handle dynamic nodes, ask for the name of the node in the dataflow, and the same values as the ENV variables.
+    parser = argparse.ArgumentParser(description="Simple arrow sender")
+
+    parser.add_argument(
+        "--name",
+        type=str,
+        required=False,
+        help="The name of the node in the dataflow.",
+        default="arrow-assert",
+    )
+    parser.add_argument(
+        "--data",
+        type=str,
+        required=False,
+        help="Arrow Data as string.",
+        default="",
+    )
+
+    args = parser.parse_args()
+
+    data = os.getenv("DATA", args.data)
+
+    node = Node(
+        args.name
+    )  # provide the name to connect to the dataflow if dynamic node
+
+    assert_data = eval(data)
+
+    for event in node:
+        if event["type"] == "INPUT":
+            value = event["value"]
+            assert value == assert_data, f"Expected {assert_data}, got {value}"
+
+
+if __name__ == "__main__":
+    main()
---
+++ node-hub/pyarrow-sender/pyarrow_sender/main.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+
+import pyarrow as pa
+
+from dora import Node
+
+RUNNER_CI = True if os.getenv("CI") == "true" else False
+
+
+def main():
+
+    # Handle dynamic nodes, ask for the name of the node in the dataflow, and the same values as the ENV variables.
+    parser = argparse.ArgumentParser(description="Simple arrow sender")
+
+    parser.add_argument(
+        "--name",
+        type=str,
+        required=False,
+        help="The name of the node in the dataflow.",
+        default="pyarrow-sender",
+    )
+    parser.add_argument(
+        "--data",
+        type=str,
+        required=False,
+        help="Arrow Data as string.",
+        default=None,
+    )
+
+    args = parser.parse_args()
+
+    data = os.getenv("DATA", args.data)
+
+    node = Node(
+        args.name
+    )  # provide the name to connect to the dataflow if dynamic node
+
+    if data is None and os.getenv("DORA_NODE_CONFIG") is None:
+        while True:
+            data = input(
+                "Provide the data you want to send:  ",
+            )
+            data = eval(data)
+            if isinstance(data, list):
+                data = pa.array(data)  # initialize pyarrow array
+            elif isinstance(data, str):
+                data = pa.array([data])
+            elif isinstance(data, int):
+                data = pa.array([data])
+            elif isinstance(data, float):
+                data = pa.array([data])
+            else:
+                data = pa.array(data)  # initialize pyarrow array
+            node.send_output("data", data)
+    else:
+        data = eval(data)
+        if isinstance(data, list):
+            data = pa.array(data)  # initialize pyarrow array
+        elif isinstance(data, str):
+            data = pa.array([data])
+        else:
+            data = pa.array(data)  # initialize pyarrow array
+        node.send_output("data", data)
+
+
+if __name__ == "__main__":
+    main()
