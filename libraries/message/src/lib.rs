22373b28ac99a40e9f910c4ca46bdebdbc1b9b3f
Philipp Oppermann <dev@phil-opp.com>
2024-08-12 19:10:38 +0200

    Version comparison: Check for semver-compatibility instead of exact match

--- libraries/message/src/lib.rs
+++ libraries/message/src/lib.rs
@@ -18,3 +18,26 @@ pub mod cli_to_coordinator;
 pub mod coordinator_to_cli;
 
 pub type DataflowId = uuid::Uuid;
+
+fn current_crate_version() -> semver::Version {
+    let crate_version_raw = env!("CARGO_PKG_VERSION");
+    let crate_version = semver::Version::parse(crate_version_raw).unwrap();
+    crate_version
+}
+
+fn versions_compatible(
+    crate_version: &semver::Version,
+    specified_version: &semver::Version,
+) -> Result<bool, String> {
+    let req = semver::VersionReq::parse(&crate_version.to_string()).map_err(|error| {
+        format!("failed to parse crate version `{crate_version}` as `VersionReq`: {error}")
+    })?;
+    let specified_dora_req = semver::VersionReq::parse(&specified_version.to_string())
+        .map_err(|error| {
+            format!(
+                "failed to parse specified dora version `{specified_version}` as `VersionReq`: {error}",
+            )
+        })?;
+    let matches = req.matches(&specified_version) || specified_dora_req.matches(crate_version);
+    Ok(matches)
+}
