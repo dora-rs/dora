8c1ea9a433ed57202e6ab1636ed35a45d717d220
haixuanTao <tao.xavier@outlook.com>
2024-09-09 16:39:56 +0200

    Remove local only that is failing dora-distil-whishper

--- node-hub/dora-distil-whisper/dora_distil_whisper/main.py
+++ node-hub/dora-distil-whisper/dora_distil_whisper/main.py
@@ -10,11 +10,7 @@ torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 model_id = "distil-whisper/distil-large-v3"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id,
-    torch_dtype=torch_dtype,
-    low_cpu_mem_usage=True,
-    use_safetensors=True,
-    local_files_only=True,
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
 )
 model.to(device)
 
