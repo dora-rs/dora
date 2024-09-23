5340dc200c9063d5e1f3c82d38e62957b6d2a27c
haixuanTao <tao.xavier@outlook.com>
2024-09-23 15:33:21 +0200

    Adding a jupyter notebook example for webcam stream

--- node-hub/opencv-plot/opencv_plot/tests/test_opencv_plot.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def test_import_main():
-    from opencv_plot.main import main
-
-    # Check that everything is working, and catch dora Runtime Exception as we're not running in a dora dataflow.
-    with pytest.raises(RuntimeError):
-        main()
