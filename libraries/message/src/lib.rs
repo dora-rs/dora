cb80f463ceb26b36a8bab6f61b458294585272b5
haixuanTao <tao.xavier@outlook.com>
2024-10-08 05:37:22 +0200

    Fix required `current_crate_version`

--- libraries/message/src/lib.rs
+++ libraries/message/src/lib.rs
@@ -19,6 +19,12 @@ pub mod coordinator_to_cli;
 
 pub type DataflowId = uuid::Uuid;
 
+fn current_crate_version() -> semver::Version {
+    let crate_version_raw = env!("CARGO_PKG_VERSION");
+    let crate_version = semver::Version::parse(crate_version_raw).unwrap();
+    crate_version
+}
+
 // Current version compatibility will makes dora message accept
 // all messages from the same major and minor version and ignore patch version.
 fn versions_compatible(
