d8573aec2b9947f5047c3024333c7eb92ff3c599
haixuanTao <tao.xavier@outlook.com>
2024-11-13 04:15:13 +0100

    Add pyorbeck into dora node hub

---
+++ node-hub/dora-pyorbbecksdk/tests/test_pyorbbecksdk.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_import_main():
+    # from opencv_video_capture.main import main
+    pass
+    # skip test as pyorbbecksdk installation is a bit complicated
+
+    # Check that everything is working, and catch dora Runtime Exception as we're not running in a dora dataflow.
+    # with pytest.raises(RuntimeError):
+    #    main()
