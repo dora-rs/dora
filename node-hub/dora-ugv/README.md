0c977c701277f415e509a0691b253ffed84f566b
haixuanTao <tao.xavier@outlook.com>
2024-11-13 08:44:15 +0100

    Adding ugv node

---
+++ node-hub/dora-ugv/README.md
@@ -0,0 +1,5 @@
+# Dora Node (Experimental) to communicate with Agilex UGV
+
+This node is used to communicate with Agilex UGV.
+
+Make sure to follow both the setup script and installation script from https://github.com/westonrobot/ugv_sdk/tree/main?tab=readme-ov-file#build-the-package-as-a-python-package
---
+++ node-hub/dora-ugv/dora_ugv/main.py
@@ -0,0 +1,52 @@
+try:
+    import ugv_sdk_py
+    from ugv_sdk_py import hunter_robot
+except ImportError as err:
+    print(
+        "Please install ugv_sdk_py first by following the instruction at: https://github.com/westonrobot/ugv_sdk/tree/main?tab=readme-ov-file#build-the-package-as-a-python-package"
+    )
+    raise err
+
+from dora import Node
+import pyarrow as pa
+
+
+def main():
+    # Create an instance of HunterRobot
+    robot = hunter_robot.HunterRobot(ugv_sdk_py.ProtocolVersion.AGX_V2)
+
+    # Connect to the robot
+    try:
+        robot.connect("can0")
+        print("Connection to the robot established.")
+    except Exception as e:
+        print(f"Failed to connect to the robot: {e}")
+        return
+
+    print("protocol version: ", robot.get_parser_protocol_version())
+    print("robot version: ", robot.request_version())
+
+    # Enable commanded mode
+    robot.enable_commanded_mode()
+    node = Node()
+    # Set motion command
+    for event in node:
+        if event["type"] == "INPUT":
+            if event["id"] == "action":
+                lin_vel, ang_vel = event["value"].to_numpy()
+                robot.set_motion_command(lin_vel, ang_vel)
+            else:
+                state = robot.get_robot_state()
+                node.send_output(
+                    "velocity",
+                    pa.array(
+                        [
+                            state.motion_state.linear_velocity,
+                            state.motion_state.angular_velocity,
+                        ]
+                    ),
+                )
+
+
+if __name__ == "__main__":
+    main()
