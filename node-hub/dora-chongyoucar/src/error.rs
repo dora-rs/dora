ad066bf72693864ae085c61391843c0c2dc1c281
Leon <echo_ai@foxmail.com>
2024-11-28 11:05:30 +0800

    feat: Added detailed information about errors
    Author: Leon <echo_ai@foxmail.com>

--- node-hub/dora-chongyoucar/src/error.rs
+++ node-hub/dora-chongyoucar/src/error.rs
@@ -2,10 +2,10 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("serial connect fail")]
-    Connect,
-    #[error("serial settings set fail")]
-    SettingsSet,
-    #[error("serial set timeout fail")]
-    SetTimeout,
+    #[error("serial: {0} connect fail")]
+    Connect(String),
+    #[error("serial settings: {0} set fail")]
+    SettingsSet(String),
+    #[error("serial set timeout: {0} fail")]
+    SetTimeout(String),
 }
