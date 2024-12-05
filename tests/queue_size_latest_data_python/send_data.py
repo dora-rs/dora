1aeaa1b5ff013bcfbf4446bdb58e54a674a269a8
haixuanTao <tao.xavier@outlook.com>
2024-12-05 15:08:33 +0100

    Adding additional test for latency

--- tests/queue_size_latest_data_python/send_data.py
+++ tests/queue_size_latest_data_python/send_data.py
@@ -5,6 +5,11 @@ import numpy as np
 
 node = Node()
 
+i = 0
 for event in node:
+    if i == 1000:
+        break
+    else:
+        i += 1
     now = time.clock_gettime_ns(0)
     node.send_output("data", pa.array([np.uint64(now)]))
