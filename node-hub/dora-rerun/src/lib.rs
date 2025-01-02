3cde7ea59894fa1c383557efaea176bd12d05ef0
haixuantao <tao.xavier@outlook.com>
2025-01-02 13:20:16 +0100

    Fix version setting and add setting up architecture in CI/CD

--- node-hub/dora-rerun/src/lib.rs
+++ node-hub/dora-rerun/src/lib.rs
@@ -307,5 +307,6 @@ fn py_main(_py: Python) -> eyre::Result<()> {
 #[pymodule]
 fn dora_rerun(_py: Python, m: Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(py_main, &m)?)?;
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }
