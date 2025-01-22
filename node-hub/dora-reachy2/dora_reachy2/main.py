69d8b0d774a04f517aa6b71ce90ade9bea5f6d63
haixuantao <tao.xavier@outlook.com>
2025-01-22 11:39:24 +0100

    Refactor reachy

--- node-hub/dora-reachy2/dora_reachy2/main.py
+++ node-hub/dora-reachy2/dora_reachy2/main.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pyarrow as pa
 from dora import Node
@@ -6,16 +8,13 @@ from reachy2_sdk.media.camera import CameraView
 
 
 def main():
-    freq = 30
-
-    ROBOT_IP = "172.17.134.85"
-    # ROBOT_IP = "localhost"
+    ROBOT_IP = os.getenv("ROBOT_IP", "172.17.134.85")
 
     reachy = ReachySDK(ROBOT_IP)
 
-    SIMULATION = False
-
     reachy.turn_on()
+
+    params = reachy.cameras.depth.get_parameters(CameraView.DEPTH)
 
     node = Node()
 
@@ -36,6 +35,7 @@ def main():
                 (image_right, _) = reachy.cameras.teleop.get_frame(
                     view=CameraView.RIGHT
                 )
+
                 node.send_output(
                     "image_right",
                     pa.array(image_right.ravel()),
@@ -45,6 +45,34 @@ def main():
                         "height": image_right.shape[0],
                     },
                 )
+
+                (depth_image, _) = reachy.cameras.depth.get_frame()
+                node.send_output(
+                    "image_depth",
+                    pa.array(depth_image.ravel()),
+                    metadata={
+                        "encoding": "bgr8",
+                        "width": depth_image.shape[1],
+                        "height": depth_image.shape[0],
+                    },
+                )
+
+                (depth_frame, _) = reachy.cameras.depth.get_depth_frame()
+
+                if params is not None and depth_frame is not None:
+                    height, width, _distortion_model, _D, K, _R, _P = params
+                    depth_frame = depth_frame.ravel().astype(np.float64()) / 1_000.0
+
+                    node.send_output(
+                        "depth",
+                        pa.array(depth_frame),
+                        metadata={
+                            "width": width,
+                            "height": height,
+                            "focal": [int(K[0, 0]), int(K[1, 1])],
+                            "resolution": [int(K[0, 2]), int(K[1, 2])],
+                        },
+                    )
 
                 position = reachy.mobile_base.get_current_odometry()
                 position = [
