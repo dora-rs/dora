a49bfb21790d2b3ba69244f5afad369e470d2918
haixuanTao <tao.xavier@outlook.com>
2025-02-07 10:41:57 +0100

    Bounding box projection

--- node-hub/dora-pointcloud-projection/src/main.rs
+++ node-hub/dora-pointcloud-projection/src/main.rs
@@ -1,5 +1,8 @@
 use dora_node_api::{
-    arrow::array::{Float64Array, UInt8Array},
+    arrow::{
+        array::{AsArray, Float64Array, UInt8Array},
+        datatypes::Int64Type,
+    },
     dora_core::config::DataId,
     DoraNode, Event, IntoArrow, Parameter,
 };
@@ -9,19 +12,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let (mut node, mut events) = DoraNode::init_from_env()?;
     let mut image_cache = HashMap::new();
 
-    let min_x = std::env::var("MIN_X").unwrap_or_default().parse::<f32>()?;
-    let max_x = std::env::var("MAX_X")
-        .unwrap_or("5.0".to_string())
-        .parse::<f32>()?;
-    let min_y = std::env::var("MIN_Y")
-        .unwrap_or("-2.0".to_string())
-        .parse::<f32>()?;
-    let max_y = std::env::var("MAX_Y")
-        .unwrap_or("2.0".to_string())
-        .parse::<f32>()?;
-    let theta_y = std::env::var("THETA_Y")
-        .unwrap_or("0.9".to_string())
-        .parse::<f32>()?;
+    let mut depth_frame = None;
+    let mut width = 640;
+    let mut height = 640;
+    let mut focal_length = vec![605, 605];
+    let mut resolution = vec![605, 605];
 
     while let Some(event) = events.recv() {
         match event {
@@ -31,27 +26,27 @@ fn main() -> Result<(), Box<dyn Error>> {
                     image_cache.insert(id.clone(), buffer.values().to_vec());
                 }
                 "depth" => {
-                    let height = if let Some(Parameter::Integer(height)) =
+                    height = if let Some(Parameter::Integer(height)) =
                         metadata.parameters.get("height")
                     {
-                        height
+                        *height
                     } else {
-                        &640
+                        height
                     };
-                    let width =
+                    width =
                         if let Some(Parameter::Integer(width)) = metadata.parameters.get("width") {
-                            width
+                            *width
                         } else {
-                            &640
+                            width
                         };
-                    let focal_length = if let Some(Parameter::ListInt(focals)) =
+                    focal_length = if let Some(Parameter::ListInt(focals)) =
                         metadata.parameters.get("focal")
                     {
                         focals.to_vec()
                     } else {
                         vec![605, 605]
                     };
-                    let resolution = if let Some(Parameter::ListInt(resolution)) =
+                    resolution = if let Some(Parameter::ListInt(resolution)) =
                         metadata.parameters.get("resolution")
                     {
                         resolution.to_vec()
@@ -59,51 +54,96 @@ fn main() -> Result<(), Box<dyn Error>> {
                         vec![640, 480]
                     };
                     let buffer: &Float64Array = data.as_any().downcast_ref().unwrap();
+                    // if let Some(color_buffer) = image_cache.get(&id.replace("depth", "image")) {
+                    // let buffer_length = color_buffer.len();
+                    // let mut new_data = vec![0; buffer_length];
+                    // buffer.iter().enumerate().for_each(|(i, z)| {
+                    // // let i = buffer.len() - i - 1; // Reverse the order of the buffer
+                    // let u = i as f32 % width as f32; // Calculate x-coordinate (u)
+                    // let v = i as f32 / width as f32; // Calculate y-coordinate (v)
+                    // let z = z.unwrap_or_default() as f32;
 
-                    if let Some(color_buffer) = image_cache.get(&id.replace("depth", "image")) {
-                        let buffer_length = color_buffer.len();
-                        let mut new_data = vec![0; buffer_length];
-                        buffer.iter().enumerate().for_each(|(i, z)| {
-                            // let i = buffer.len() - i - 1; // Reverse the order of the buffer
-                            let u = i as f32 % *width as f32; // Calculate x-coordinate (u)
-                            let v = i as f32 / *width as f32; // Calculate y-coordinate (v)
-                            let z = z.unwrap_or_default() as f32;
+                    // let y = (u - resolution[0] as f32) * z / focal_length[0] as f32;
+                    // let x = (v - resolution[1] as f32) * z / focal_length[1] as f32;
 
-                            let y = (u - resolution[0] as f32) * z / focal_length[0] as f32;
-                            let x = (v - resolution[1] as f32) * z / focal_length[1] as f32;
+                    // let tmp_x = z * theta_y.cos() - x * theta_y.sin();
+                    // let new_u =
+                    // height as f32 - ((tmp_x - min_x) / (max_x - min_x) * height as f32);
+                    // let new_v = (y - min_y) / (max_y - min_y) * width as f32;
 
-                            let tmp_x = z * theta_y.cos() - x * theta_y.sin();
-                            let new_u = *height as f32
-                                - ((tmp_x - min_x) / (max_x - min_x) * *height as f32);
-                            let new_v = (y - min_y) / (max_y - min_y) * *width as f32;
+                    // let new_u = new_u.round();
+                    // let new_v = new_v.round();
 
-                            let new_u = new_u.round();
-                            let new_v = new_v.round();
+                    // // verify if the point is within the bounds
+                    // if new_u >= 0.0
+                    // && new_u < height as f32
+                    // && new_v >= 0.0
+                    // && new_v < width as f32
+                    // {
+                    // let new_i = (new_v + new_u * width as f32) as usize;
+                    // if new_data[new_i * 3] == 0 {
+                    // new_data[new_i * 3] = color_buffer[i * 3];
+                    // new_data[new_i * 3 + 1] = color_buffer[i * 3 + 1];
+                    // new_data[new_i * 3 + 2] = color_buffer[i * 3 + 2];
+                    // }
+                    // }
+                    // });
 
-                            // verify if the point is within the bounds
-                            if new_u >= 0.0
-                                && new_u < *height as f32
-                                && new_v >= 0.0
-                                && new_v < *width as f32
-                            {
-                                let new_i = (new_v + new_u * *width as f32) as usize;
-                                if new_data[new_i * 3] == 0 {
-                                    new_data[new_i * 3] = color_buffer[i * 3];
-                                    new_data[new_i * 3 + 1] = color_buffer[i * 3 + 1];
-                                    new_data[new_i * 3 + 2] = color_buffer[i * 3 + 2];
+                    // let mut metadata = metadata.parameters.clone();
+                    // metadata.insert(
+                    // "encoding".to_string(),
+                    // parameter::string("bgr8".to_string()),
+                    // );
+                    // node.send_output(
+                    // dataid::from("image".to_string()),
+                    // metadata,
+                    // new_data.into_arrow(),
+                    // )?;
+                    depth_frame = Some(buffer.clone());
+                }
+                "boxes2d" => {
+                    if let Some(data) = data.as_primitive_opt::<Int64Type>() {
+                        let data = data.values();
+                        println!("data: {data:#?}");
+                        let x_min = data[0] as f32;
+                        let y_min = data[1] as f32;
+                        let x_max = data[2] as f32;
+                        let y_max = data[3] as f32;
+                        let mut x_vec = vec![];
+                        let mut y_vec = vec![];
+                        let mut z_vec = vec![];
+
+                        if let Some(depth_frame) = &depth_frame {
+                            depth_frame.iter().enumerate().for_each(|(i, z)| {
+                                let u = i as f32 % width as f32; // Calculate x-coordinate (u)
+                                let v = i as f32 / width as f32; // Calculate y-coordinate (v)
+
+                                if u > x_min && u < x_max && v > y_min && v < y_max {
+                                    let z = z.unwrap_or_default() as f32;
+
+                                    let y = (u - resolution[0] as f32) * z / focal_length[0] as f32;
+                                    let x = (v - resolution[1] as f32) * z / focal_length[1] as f32;
+                                    x_vec.push(x);
+                                    y_vec.push(y);
+                                    z_vec.push(z);
                                 }
-                            }
-                        });
+                            });
+                        }
+                        let x_mean = x_vec.iter().sum::<f32>() / x_vec.len() as f32;
+                        let y_mean = y_vec.iter().sum::<f32>() / x_vec.len() as f32;
+                        let z_mean = z_vec.iter().sum::<f32>() / x_vec.len() as f32;
 
-                        let mut metadata = metadata.parameters.clone();
-                        metadata.insert(
-                            "encoding".to_string(),
-                            Parameter::String("bgr8".to_string()),
-                        );
+                        let cos_theta = -0.78; // np.cos(np.deg2rad(180-38))
+                        let sin_theta = 0.61; // np.sin(np.deg2rad(180-38))
+
+                        let adj_x = sin_theta * z_mean + cos_theta * x_mean;
+                        let adj_y = y_mean;
+                        let adj_z = cos_theta * z_mean + sin_theta * x_mean;
+                        let metadata = metadata.parameters.clone();
                         node.send_output(
-                            DataId::from("image".to_string()),
+                            DataId::from("point".to_string()),
                             metadata,
-                            new_data.into_arrow(),
+                            vec![adj_x, adj_y, adj_z].into_arrow(),
                         )?;
                     }
                 }
