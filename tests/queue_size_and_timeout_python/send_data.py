c39316f65c9362afbab6bd896fc3f2dd472fd557
haixuantao <tao.xavier@outlook.com>
2025-02-23 15:33:09 +0100

    Fix scheduler not being used when called through node timeout

--- tests/queue_size_and_timeout_python/send_data.py
+++ tests/queue_size_and_timeout_python/send_data.py
@@ -8,10 +8,12 @@ def main() -> None:
     dora_node = Node()
     i = 0
     while True:
-        dora_node.send_output("ts", pa.array([time.perf_counter_ns()]))
+        dora_node.send_output("ts", pa.array([time.perf_counter_ns(), i]))
         i += 1
         # print(f"Sent {i} times", flush=True)
         time.sleep(0.001)
+        if dora_node.next(timeout=0.001) is None:
+            break
 
 
 if __name__ == "__main__":
