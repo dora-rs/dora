ab5363c62f1d710a8a173497b946b11b5c1b68a0
haixuantao <tao.xavier@outlook.com>
2025-02-24 09:48:16 +0100

    Fix version incompatibility by restoring orders of metadata parameters

--- libraries/message/src/metadata.rs
+++ libraries/message/src/metadata.rs
@@ -55,13 +55,16 @@ pub struct ArrowTypeInfo {
     pub child_data: Vec<ArrowTypeInfo>,
 }
 
+// WARNING
+// Any change of parameters should be appended at the end of the enum
+// otherwise it will break compatibility with previous dora version
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub enum Parameter {
     Bool(bool),
     Integer(i64),
-    Float(f64),
     String(String),
     ListInt(Vec<i64>),
+    Float(f64),
     ListFloat(Vec<f64>),
 }
 
