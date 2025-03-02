4d0770f343ce37f46df7c88b93e14b8e8120439a
haixuantao <tao.xavier@outlook.com>
2025-03-02 10:03:35 +0100

    Fix typo

--- binaries/daemon/src/node_communication/tcp.rs
+++ binaries/daemon/src/node_communication/tcp.rs
@@ -75,7 +75,7 @@ impl Connection for TcpConnection {
             },
         };
         bincode::deserialize(&raw)
-            .with_context(|| "failed to deserialize abc DaemonRequest")
+            .wrap_err("failed to deserialize DaemonRequest")
             .map(Some)
     }
 
