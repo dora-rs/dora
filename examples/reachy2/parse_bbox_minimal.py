196fa8e9e47f62ab3f605b2f7889c4c091918a0f
Mati-ur-rehman-017 <matiurrehman017@gmail.com>
2025-03-10 21:08:48 +0500

    Fixed D417 and remove file

--- examples/reachy2/parse_bbox_minimal.py
+++ examples/reachy2/parse_bbox_minimal.py
@@ -17,7 +17,8 @@ def extract_bboxes(json_text) -> (np.ndarray, np.ndarray):
 
     Parameters
     ----------
-    json_text (str): JSON string containing bounding box data, including ```json markers.
+    json_text : str
+        JSON string containing bounding box data, including ```json markers.
 
     Returns
     -------
