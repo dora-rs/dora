c57824af8ff7a7723d56136bf2508609d90bf62e
ShashwatPatil <shashwatpatil974@gmail.com>
2025-03-17 00:09:41 +0530

    renamed package to dora-transformers

--- node-hub/dora-transformers/tests/test_dora_transformers.py
+++ node-hub/dora-transformers/tests/test_dora_transformers.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_import_main():
-    from dora_transformer.main import main
+    from dora_transformers.main import main
 
     # Check that everything is working, and catch dora Runtime Exception as we're not running in a dora dataflow.
     with pytest.raises(RuntimeError):
