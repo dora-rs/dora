ad98015ba1de52924da73c563a53f7e17fe6e529
krishna dubey <krishnadubey1008@gmail.com>
2025-03-16 03:35:39 +0000

    fixed error due to Node class

--- node-hub/dora-lmdeploy/tests/test_dora_lmdeploy.py
+++ node-hub/dora-lmdeploy/tests/test_dora_lmdeploy.py
@@ -1,12 +1,9 @@
-"""Tests for the dora-lmdeploy node."""
-
 import pytest
-
 
 def test_import_main():
     """Test that the main function in dora-lmdeploy can be imported and executed."""
     from dora_lmdeploy.main import main
 
-    # Check that everything is working, and catch Dora RuntimeException as we're not running in a Dora dataflow.
-    with pytest.raises(RuntimeError):
+    with pytest.raises(RuntimeError, match=".*Node.*"):
         main()
+
