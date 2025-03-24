794087089539817c6683dcd9279447074236f517
Haroon Tahir <haroon152018@gmail.com>
2025-03-24 19:49:40 +0500

    Changed code to only use eyre and workspace version of dora-node-api

--- node-hub/dora-mistral-rs/src/main.rs
+++ node-hub/dora-mistral-rs/src/main.rs
@@ -1,29 +1,31 @@
-use anyhow::{anyhow, Result};
 use dora_node_api::{dora_core::config::DataId, DoraNode, Event, IntoArrow};
-use eyre::Context;
+use eyre::{Context, Report};
 use mistralrs::{
     IsqType, PagedAttentionMetaBuilder, TextMessageRole, TextMessages, TextModelBuilder,
 };
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let (mut node, mut events) = DoraNode::init_from_env().map_err(|e| anyhow!(e))?;
+async fn main() -> eyre::Result<()> {
+    let (mut node, mut events) = DoraNode::init_from_env()?;
     let mistral_output = DataId::from("mistralrs".to_owned());
 
     let model = TextModelBuilder::new("deepseek-ai/DeepSeek-R1")
         .with_isq(IsqType::Q4K)
         .with_logging()
-        .with_paged_attn(|| PagedAttentionMetaBuilder::default().build())?
+        .with_paged_attn(|| PagedAttentionMetaBuilder::default().build())
+        .map_err(|e| Report::msg(format!("Paged attention meta error: {}", e))) // Convert error
+        .expect("Failed to build model")
         .build()
-        .await?;
+        .await
+        .map_err(|e| Report::msg(format!("Model Build error: {}", e))) // Convert error
+        .expect("Failed to build model");
 
     while let Some(event) = events.recv() {
         match event {
             Event::Input { id, metadata, data } => match id.as_str() {
                 "message" => {
-                    let received_string: &str = TryFrom::try_from(&data)
-                        .context("expected string message")
-                        .map_err(|e| anyhow!(e))?;
+                    let received_string: &str =
+                        TryFrom::try_from(&data).context("expected string message")?;
                     eprintln!("Received message: {}", received_string);
 
                     let messages = TextMessages::new()
@@ -33,7 +35,12 @@ async fn main() -> Result<()> {
                         )
                         .add_message(TextMessageRole::User, received_string);
 
-                    let response = model.send_chat_request(messages).await?;
+                    let response = model
+                        .send_chat_request(messages)
+                        .await
+                        .map_err(|e| Report::msg(format!("Model Response error: {}", e))) // Convert error
+                        .expect("Failed to get response from model");
+
                     let output = response.choices[0].message.content.as_ref().unwrap();
                     println!("{}", output);
 
@@ -46,8 +53,7 @@ async fn main() -> Result<()> {
                         mistral_output.clone(),
                         metadata.parameters,
                         output.into_arrow(),
-                    )
-                    .map_err(|e| anyhow!(e))?;
+                    )?;
                 }
                 other => eprintln!("Received input `{other}`"),
             },
