841ef960b4a18030c77faf375e7b71091dba5afe
haixuantao <tao.xavier@outlook.com>
2025-03-25 13:57:22 +0100

    Fixing minor things and adding an example of mistral rs benches

--- node-hub/dora-mistral-rs/src/main.rs
+++ node-hub/dora-mistral-rs/src/main.rs
@@ -1,38 +1,33 @@
 use dora_node_api::{dora_core::config::DataId, DoraNode, Event, IntoArrow};
 use eyre::{Context, Report};
-use mistralrs::{
-    IsqType, PagedAttentionMetaBuilder, TextMessageRole, TextMessages, TextModelBuilder,
-};
+use mistralrs::{TextMessageRole, TextMessages, TextModelBuilder};
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let (mut node, mut events) = DoraNode::init_from_env()?;
-    let mistral_output = DataId::from("mistralrs".to_owned());
+    let mistral_output = DataId::from("text".to_owned());
+    let model_path = std::env::var("MODEL_PATH_OR_NAME")
+        .unwrap_or_else(|_| "Qwen/Qwen2.5-0.5B-Instruct".to_owned());
 
-    let model = TextModelBuilder::new("deepseek-ai/DeepSeek-R1")
-        .with_isq(IsqType::Q4K)
+    let model = TextModelBuilder::new(model_path)
         .with_logging()
-        .with_paged_attn(|| PagedAttentionMetaBuilder::default().build())
-        .map_err(|e| Report::msg(format!("Paged attention meta error: {}", e))) // Convert error
-        .expect("Failed to build model")
         .build()
         .await
         .map_err(|e| Report::msg(format!("Model Build error: {}", e))) // Convert error
         .expect("Failed to build model");
 
-    while let Some(event) = events.recv() {
+    while let Some(event) = events.recv_async().await {
         match event {
             Event::Input { id, metadata, data } => match id.as_str() {
-                "message" => {
+                "text" => {
                     let received_string: &str =
                         TryFrom::try_from(&data).context("expected string message")?;
-                    eprintln!("Received message: {}", received_string);
 
                     let messages = TextMessages::new()
-                        .add_message(
-                            TextMessageRole::System,
-                            "You are an AI agent with a specialty in robotics.",
-                        )
+                        // .add_message(
+                        // TextMessageRole::System,
+                        // "You are an AI agent with a specialty in robotics.",
+                        // )
                         .add_message(TextMessageRole::User, received_string);
 
                     let response = model
@@ -42,12 +37,6 @@ async fn main() -> eyre::Result<()> {
                         .expect("Failed to get response from model");
 
                     let output = response.choices[0].message.content.as_ref().unwrap();
-                    println!("{}", output);
-
-                    dbg!(
-                        response.usage.avg_prompt_tok_per_sec,
-                        response.usage.avg_compl_tok_per_sec
-                    );
 
                     node.send_output(
                         mistral_output.clone(),
