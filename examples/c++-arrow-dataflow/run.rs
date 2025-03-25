85731cc23c65d037311d3576d22bc399a883bc1e
haixuantao <tao.xavier@outlook.com>
2025-03-25 18:15:34 +0100

    Improve readme and improve name of the example

--- examples/c++-arrow-dataflow/run.rs
+++ examples/c++-arrow-dataflow/run.rs
@@ -1,10 +1,6 @@
 use dora_tracing::set_up_tracing;
 use eyre::{bail, Context};
-use std::{
-    env::consts::{DLL_PREFIX, DLL_SUFFIX, EXE_SUFFIX},
-    path::Path,
-    process::Command,
-};
+use std::{env::consts::EXE_SUFFIX, path::Path, process::Command};
 
 struct ArrowConfig {
     cflags: String,
@@ -52,30 +48,7 @@ async fn main() -> eyre::Result<()> {
         build_dir.join("dora-node-api.h"),
     )
     .await?;
-    tokio::fs::write(
-        build_dir.join("operator.h"),
-        r###"#include "../operator-rust-api/operator.h""###,
-    )
-    .await?;
 
-    build_package("dora-operator-api-cxx").await?;
-    let operator_cxxbridge = target
-        .join("cxxbridge")
-        .join("dora-operator-api-cxx")
-        .join("src");
-    tokio::fs::copy(
-        operator_cxxbridge.join("lib.rs.cc"),
-        build_dir.join("operator-bridge.cc"),
-    )
-    .await?;
-    tokio::fs::copy(
-        operator_cxxbridge.join("lib.rs.h"),
-        build_dir.join("dora-operator-api.h"),
-    )
-    .await?;
-
-    build_package("dora-node-api-c").await?;
-    build_package("dora-operator-api-c").await?;
     build_cxx_node(
         root,
         &[
@@ -91,47 +64,7 @@ async fn main() -> eyre::Result<()> {
         ],
     )
     .await?;
-    build_cxx_node(
-        root,
-        &[&dunce::canonicalize(
-            Path::new("node-c-api").join("main.cc"),
-        )?],
-        "node_c_api",
-        &["-l", "dora_node_api_c"],
-    )
-    .await?;
-    build_cxx_operator(
-        &[
-            &dunce::canonicalize(Path::new("operator-rust-api").join("operator.cc"))?,
-            &dunce::canonicalize(build_dir.join("operator-bridge.cc"))?,
-        ],
-        "operator_rust_api",
-        &[
-            "-l",
-            "dora_operator_api_cxx",
-            "-L",
-            root.join("target").join("debug").to_str().unwrap(),
-            &arrow_config.cflags,
-            &arrow_config.libs,
-        ],
-    )
-    .await?;
-    build_cxx_operator(
-        &[&dunce::canonicalize(
-            Path::new("operator-c-api").join("operator.cc"),
-        )?],
-        "operator_c_api",
-        &[
-            "-l",
-            "dora_operator_api_c",
-            "-L",
-            root.join("target").join("debug").to_str().unwrap(),
-        ],
-    )
-    .await?;
-
     let dataflow = Path::new("dataflow.yml").to_owned();
-    build_package("dora-runtime").await?;
     run_dataflow(&dataflow).await?;
 
     Ok(())
@@ -267,95 +200,5 @@ async fn build_cxx_node(
     if !clang.status().await?.success() {
         bail!("failed to compile c++ node");
     };
-    Ok(())
-}
-
-async fn build_cxx_operator(
-    paths: &[&Path],
-    out_name: &str,
-    link_args: &[&str],
-) -> eyre::Result<()> {
-    let mut object_file_paths = Vec::new();
-
-    for path in paths {
-        let mut compile = tokio::process::Command::new("clang++");
-        compile.arg("-c").arg(path);
-        compile.arg("-std=c++17");
-        let object_file_path = path.with_extension("o");
-        compile.arg("-o").arg(&object_file_path);
-        #[cfg(unix)]
-        compile.arg("-fPIC");
-        if let Some(parent) = path.parent() {
-            compile.current_dir(parent);
-        }
-        if !compile.status().await?.success() {
-            bail!("failed to compile cxx operator");
-        };
-        object_file_paths.push(object_file_path);
-    }
-
-    let mut link = tokio::process::Command::new("clang++");
-    link.arg("-shared").args(&object_file_paths);
-    for arg in link_args {
-        if arg.contains(" ") {
-            for part in arg.split_whitespace() {
-                link.arg(part);
-            }
-        } else {
-            link.arg(arg);
-        }
-    }
-    #[cfg(target_os = "windows")]
-    {
-        link.arg("-ladvapi32");
-        link.arg("-luserenv");
-        link.arg("-lkernel32");
-        link.arg("-lws2_32");
-        link.arg("-lbcrypt");
-        link.arg("-lncrypt");
-        link.arg("-lschannel");
-        link.arg("-lntdll");
-        link.arg("-liphlpapi");
-
-        link.arg("-lcfgmgr32");
-        link.arg("-lcredui");
-        link.arg("-lcrypt32");
-        link.arg("-lcryptnet");
-        link.arg("-lfwpuclnt");
-        link.arg("-lgdi32");
-        link.arg("-lmsimg32");
-        link.arg("-lmswsock");
-        link.arg("-lole32");
-        link.arg("-lopengl32");
-        link.arg("-lsecur32");
-        link.arg("-lshell32");
-        link.arg("-lsynchronization");
-        link.arg("-luser32");
-        link.arg("-lwinspool");
-
-        link.arg("-Wl,-nodefaultlib:libcmt");
-        link.arg("-D_DLL");
-        link.arg("-lmsvcrt");
-        link.arg("-fms-runtime-lib=static");
-    }
-    #[cfg(target_os = "macos")]
-    {
-        link.arg("-framework").arg("CoreServices");
-        link.arg("-framework").arg("Security");
-        link.arg("-l").arg("System");
-        link.arg("-l").arg("resolv");
-        link.arg("-l").arg("pthread");
-        link.arg("-l").arg("c");
-        link.arg("-l").arg("m");
-    }
-    link.arg("-o")
-        .arg(Path::new("../build").join(format!("{DLL_PREFIX}{out_name}{DLL_SUFFIX}")));
-    if let Some(parent) = paths[0].parent() {
-        link.current_dir(parent);
-    }
-    if !link.status().await?.success() {
-        bail!("failed to create shared library from cxx operator (c api)");
-    };
-
     Ok(())
 }
