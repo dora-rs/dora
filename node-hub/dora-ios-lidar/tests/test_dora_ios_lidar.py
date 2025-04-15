b5798e9e639d07bc588e9a2ae61bb5a42f3f5e3c
Nagesh Mandal <106006006+NageshMandal@users.noreply.github.com>
2025-04-15 10:49:17 +0530

    Update test_dora_ios_lidar.py

--- node-hub/dora-ios-lidar/tests/test_dora_ios_lidar.py
+++ node-hub/dora-ios-lidar/tests/test_dora_ios_lidar.py
@@ -8,6 +8,6 @@ def test_import_main():
     from dora_ios_lidar.main import main
 
     # Check that everything is working, and catch the dora Runtime Exception 
-# since we're not running in a dora dataflow.
+    # since we're not running in a dora dataflow.
     with pytest.raises(RuntimeError):
         main()
