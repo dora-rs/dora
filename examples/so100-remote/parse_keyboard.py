521b1a54c0af155c4a9510e8028dc5e079cc5ba5
haixuanTao <tao.xavier@outlook.com>
2025-04-28 14:16:05 +0200

    Use fixed urdf to do ik

--- examples/so100-remote/parse_keyboard.py
+++ examples/so100-remote/parse_keyboard.py
@@ -14,16 +14,11 @@ now = time.time()
 for j in range(0, 100):
     for i in range(110):
         time.sleep(0.033)
-        x = 0.0 + (j * 0.005)
-        y = -0.1 - (i * 0.001)
-        if x == 0:
-            theta = -1.8
-        else:
-            theta = np.arctan(y / x)
+        y = 0.0 + (j * 0.1)
+        x = -0.1 - (i * 0.01)
 
-        print("Sending action: ", x, y, theta)
         node.send_output(
             "action",
-            pa.array([0.0 + (j * 0.005), -0.1 - (i * 0.001), 0.18, -3.1, theta, 0.45]),
+            pa.array([0.0 + (j * 0.05), -0.1 - (i * 0.001), 0.1, -1.6, -0.9, -3.14]),
             metadata={"encoding": "xyzrpy"},
         )
