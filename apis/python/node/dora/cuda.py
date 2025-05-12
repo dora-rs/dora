18bb7938d59af9afa32b00976c86d1b8804a2302
mivik <mivikq@gmail.com>
2025-05-12 23:27:32 +0800

    feat(python): refactor python CUDA IPC API

--- apis/python/node/dora/cuda.py
+++ apis/python/node/dora/cuda.py
@@ -8,13 +8,19 @@ from numba.cuda import to_device
 
 # Make sure to install numba with cuda
 from numba.cuda.cudadrv.devicearray import DeviceNDArray
+from numba.cuda.cudadrv.devices import get_context
+from numba.cuda.cudadrv.driver import IpcHandle
 
-# To install pyarrow.cuda, run `conda install pyarrow "arrow-cpp-proc=*=cuda" -c conda-forge`
-from pyarrow import cuda
+
+import pickle
+
+from contextlib import contextmanager
+from typing import ContextManager
 
 
 def torch_to_ipc_buffer(tensor: torch.TensorType) -> tuple[pa.array, dict]:
-    """Convert a Pytorch tensor into a pyarrow buffer containing the IPC handle and its metadata.
+    """Convert a Pytorch tensor into a pyarrow buffer containing the IPC handle
+    and its metadata.
 
     Example Use:
     ```python
@@ -24,75 +30,56 @@ def torch_to_ipc_buffer(tensor: torch.TensorType) -> tuple[pa.array, dict]:
     ```
     """
     device_arr = to_device(tensor)
-    cuda_buf = pa.cuda.CudaBuffer.from_numba(device_arr.gpu_data)
-    handle_buffer = cuda_buf.export_for_ipc().serialize()
+    ipch = get_context().get_ipc_handle(device_arr.gpu_data)
     metadata = {
         "shape": device_arr.shape,
         "strides": device_arr.strides,
         "dtype": device_arr.dtype.str,
     }
-    return pa.array(handle_buffer, type=pa.uint8()), metadata
+    return pa.array(pickle.dumps(ipch), type=pa.uint8()), metadata
 
 
-def ipc_buffer_to_ipc_handle(handle_buffer: pa.array) -> cuda.IpcMemHandle:
-    """Convert a buffer containing a serialized handler into cuda IPC MemHandle.
+def ipc_buffer_to_ipc_handle(handle_buffer: pa.array) -> IpcHandle:
+    """Convert a buffer containing a serialized handler into cuda IPC Handle.
 
     example use:
     ```python
+    from dora.cuda import ipc_buffer_to_ipc_handle, open_ipc_handle
 
-    import pyarrow as pa
-    from dora.cuda import ipc_buffer_to_ipc_handle, cudabuffer_to_torch
-
-    ctx = pa.cuda.context()
     event = node.next()
 
     ipc_handle = ipc_buffer_to_ipc_handle(event["value"])
-    cudabuffer = ctx.open_ipc_buffer(ipc_handle)
-    torch_tensor = cudabuffer_to_torch(cudabuffer, event["metadata"])  # on cuda
+    with open_ipc_handle(ipc_handle, event["metadata"]) as tensor:
+        pass
     ```
     """
     handle_buffer = handle_buffer.buffers()[1]
-    return pa.cuda.IpcMemHandle.from_buffer(handle_buffer)
+    return pickle.loads(handle_buffer.to_pybytes())
 
 
-def cudabuffer_to_numba(buffer: cuda.CudaBuffer, metadata: dict) -> DeviceNDArray:
-    """Convert a pyarrow CUDA buffer to numba.
+@contextmanager
+def open_ipc_handle(
+    ipc_handle: IpcHandle, metadata: dict
+) -> ContextManager[torch.TensorType]:
+    """Open a CUDA IPC handle and return a Pytorch tensor.
 
     example use:
     ```python
+    from dora.cuda import ipc_buffer_to_ipc_handle, open_ipc_handle
 
-    import pyarrow as pa
-    from dora.cuda import ipc_buffer_to_ipc_handle, cudabuffer_to_torch
-
-    ctx = pa.cuda.context()
     event = node.next()
 
     ipc_handle = ipc_buffer_to_ipc_handle(event["value"])
-    cudabuffer = ctx.open_ipc_buffer(ipc_handle)
-    numba_tensor = cudabuffer_to_numbda(cudabuffer, event["metadata"])
+    with open_ipc_handle(ipc_handle, event["metadata"]) as tensor:
+        pass
     ```
     """
     shape = metadata["shape"]
     strides = metadata["strides"]
     dtype = metadata["dtype"]
-    return DeviceNDArray(shape, strides, dtype, gpu_data=buffer.to_numba())
-
-
-def cudabuffer_to_torch(buffer: cuda.CudaBuffer, metadata: dict) -> torch.Tensor:
-    """Convert a pyarrow CUDA buffer to a torch tensor.
-
-    example use:
-    ```python
-
-    import pyarrow as pa
-    from dora.cuda import ipc_buffer_to_ipc_handle, cudabuffer_to_torch
-
-    ctx = pa.cuda.context()
-    event = node.next()
-
-    ipc_handle = ipc_buffer_to_ipc_handle(event["value"])
-    cudabuffer = ctx.open_ipc_buffer(ipc_handle)
-    torch_tensor = cudabuffer_to_torch(cudabuffer, event["metadata"])  # on cuda
-    ```
-    """
-    return torch.as_tensor(cudabuffer_to_numba(buffer, metadata), device="cuda")
+    try:
+        buffer = ipc_handle.open(get_context())
+        device_arr = DeviceNDArray(shape, strides, dtype, gpu_data=buffer)
+        yield torch.as_tensor(device_arr, device="cuda")
+    finally:
+        ipc_handle.close()
