a473d657eb8497e9eab300376c2041d9166864ae
Philipp Oppermann <dev@phil-opp.com>
2025-06-18 13:24:09 +0200

    Add `dora-session.yaml` to `.gitignore` to avoid accidental commits
    The file stores local state and should not be commited to git repositories. By adding the file to a `.gitignore` we can avoid this.
    
    Creates a new local `.gitignore` file if none exists. Otherwise, it appends the file to the existing `.gitignore` file.

--- binaries/cli/src/session.rs
+++ binaries/cli/src/session.rs
@@ -44,8 +44,28 @@ impl DataflowSession {
 
     pub fn write_out_for_dataflow(&self, dataflow_path: &Path) -> eyre::Result<()> {
         let session_file = session_file_path(dataflow_path)?;
-        std::fs::write(session_file, self.serialize()?)
+        let filename = session_file
+            .file_name()
+            .context("session file has no file name")?
+            .to_str()
+            .context("session file name is no utf8")?;
+        std::fs::write(&session_file, self.serialize()?)
             .context("failed to write dataflow session file")?;
+        let gitignore = session_file.with_file_name(".gitignore");
+        if gitignore.exists() {
+            let existing =
+                std::fs::read_to_string(&gitignore).context("failed to read gitignore")?;
+            if !existing
+                .lines()
+                .any(|l| l.split_once('/') == Some(("", filename)))
+            {
+                let new = existing + &format!("\n/{filename}\n");
+                std::fs::write(gitignore, new).context("failed to update gitignore")?;
+            }
+        } else {
+            std::fs::write(gitignore, format!("/{filename}\n"))
+                .context("failed to write gitignore")?;
+        }
         Ok(())
     }
 
@@ -55,7 +75,7 @@ impl DataflowSession {
 }
 
 fn deserialize(session_file: &Path) -> eyre::Result<DataflowSession> {
-    std::fs::read_to_string(&session_file)
+    std::fs::read_to_string(session_file)
         .context("failed to read DataflowSession file")
         .and_then(|s| {
             serde_yaml::from_str(&s).context("failed to deserialize DataflowSession file")
