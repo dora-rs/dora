d6edb85e6582a42b72a8c1da811e0b8285138536
haixuantao <tao.xavier@outlook.com>
2025-08-02 00:00:46 +0800

    Improvement of the overall language pipeline to be more resilient

--- node-hub/dora-kokoro-tts/dora_kokoro_tts/main.py
+++ node-hub/dora-kokoro-tts/dora_kokoro_tts/main.py
@@ -1,4 +1,5 @@
 """TODO: Add docstring."""
+
 import os
 import re
 
@@ -8,11 +9,12 @@ from kokoro import KPipeline
 
 LANGUAGE = os.getenv("LANGUAGE", "en")
 
+
 def main():
     """TODO: Add docstring."""
     if LANGUAGE in ["en", "english"]:
         pipeline = KPipeline(lang_code="a")
-    elif LANGUAGE in ["zh","ch","chinese"]:
+    elif LANGUAGE in ["zh", "ch", "chinese"]:
         pipeline = KPipeline(lang_code="z")
     else:
         print("warning: Defaulting to english speaker as language not found")
@@ -22,22 +24,23 @@ def main():
 
     for event in node:
         if event["type"] == "INPUT":
-            if event["id"] == "text":
-                text = event["value"][0].as_py()
-                if re.findall(r'[\u4e00-\u9fff]+', text):
-                    pipeline = KPipeline(lang_code="z")
-                elif pipeline.lang_code != "a":
-                    pipeline = KPipeline(lang_code="a")  # <= make sure lang_code matches voice
+            text = event["value"][0].as_py()
+            if re.findall(r"[\u4e00-\u9fff]+", text):
+                pipeline = KPipeline(lang_code="z")
+            elif pipeline.lang_code != "a":
+                pipeline = KPipeline(
+                    lang_code="a"
+                )  # <= make sure lang_code matches voice
 
-                generator = pipeline(
-                    text,
-                    voice="af_heart",  # <= change voice here
-                    speed=1.2,
-                    split_pattern=r"\n+",
-                )
-                for _, (_, _, audio) in enumerate(generator):
-                    audio = audio.numpy()
-                    node.send_output("audio", pa.array(audio), {"sample_rate": 24000})
+            generator = pipeline(
+                text,
+                voice="af_heart",  # <= change voice here
+                speed=1.2,
+                split_pattern=r"\n+",
+            )
+            for _, (_, _, audio) in enumerate(generator):
+                audio = audio.numpy()
+                node.send_output("audio", pa.array(audio), {"sample_rate": 24000})
 
 
 if __name__ == "__main__":
