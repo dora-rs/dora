7be307e0ab4e7948e874c49e989de1605ed2534b
haixuantao <tao.xavier@outlook.com>
2025-08-15 17:15:51 +0800

    Improve speech detection messages

--- node-hub/dora-vad/dora_vad/main.py
+++ node-hub/dora-vad/dora_vad/main.py
@@ -41,7 +41,20 @@ def main():
             if len(speech_timestamps) == 0:
                 # If there is no speech, return the audio
                 continue
-            arg_max = np.argmax([ts["end"] - ts["start"] for ts in speech_timestamps])
+            # Send a timestamp_start
+            node.send_output(
+                "timestamp_start",
+                pa.array([speech_timestamps[0]["start"]]),
+                metadata={"sample_rate": sr},
+            )
+
+            for i, ts in enumerate(reversed(speech_timestamps)):
+                # Convert timestamps to milliseconds
+                speech_timestamps[i]["start"] = int(ts["start"] / sr * 1000)
+                speech_timestamps[i]["end"] = int(ts["end"] / sr * 1000)
+                if (ts["end"] - ts["start"]) > MIN_SPEECH_DURATION_MS:
+                    arg_max = len(speech_timestamps) - i
+                    break
             # Check ig there is timestamp
             if (
                 len(speech_timestamps) > 0
@@ -51,7 +64,7 @@ def main():
                 > MIN_AUDIO_SAMPLING_DURATION_MS * sr / 1000
                 and (
                     (len(audio) - speech_timestamps[arg_max]["end"])
-                    > MIN_SILENCE_DURATION_MS / 1000 * sr * 5
+                    > MIN_SILENCE_DURATION_MS / 1000 * sr
                 )
             ):
                 # Check if the audio is not cut at the end. And only return if there is a long time spent
