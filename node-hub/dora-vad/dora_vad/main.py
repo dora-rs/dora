2d66081e2cb87def718409e49f4de59adfd899d7
haixuantao <tao.xavier@outlook.com>
2025-09-09 10:37:39 +0200

    Accumulates audio in vad to not get over saturated by audio chunk

--- node-hub/dora-vad/dora_vad/main.py
+++ node-hub/dora-vad/dora_vad/main.py
@@ -20,16 +20,30 @@ def main():
     """TODO: Add docstring."""
     node = Node()
     last_audios = []
+    tmp_audio = []
     while True:
         event = node.next()
         if event is None:
             break
         if event["type"] == "INPUT" and event["id"] == "audio":
             audio = event["value"].to_numpy()
-            last_audios += [audio]
-            last_audios = last_audios[-100:]
-            audio = np.concatenate(last_audios)
             sr = event["metadata"].get("sample_rate", 16000)
+
+            tmp_audio += [audio]
+            if (len(tmp_audio) * len(audio) / (sr)) < 0.1:
+                continue
+            audio = np.concatenate(tmp_audio)
+            last_audios += [audio]
+            tmp_audio = []
+
+            if ((10 * sr) // len(audio)) < len(last_audios):
+                print("Too long audio, trimming")
+                last_audios = last_audios[-(10 * sr) // len(audio) :]
+
+            # If the block of audio are less than 100ms skip some of them
+            if (len(audio) / sr) < 0.1:
+                continue
+            audio = np.concatenate(last_audios)
             speech_timestamps = get_speech_timestamps(
                 torch.from_numpy(audio),
                 model,
@@ -37,48 +51,19 @@ def main():
                 min_speech_duration_ms=MIN_SPEECH_DURATION_MS,
                 min_silence_duration_ms=MIN_SILENCE_DURATION_MS,
                 sampling_rate=sr,
+                speech_pad_ms=200,
             )
             if len(speech_timestamps) == 0:
                 # If there is no speech, return the audio
                 continue
             # Send a timestamp_start
+
+            # Check ig there is timestamp
             node.send_output(
-                "timestamp_start",
-                pa.array([speech_timestamps[0]["start"]]),
+                "audio",
+                pa.array(
+                    audio  # [speech_timestamps[0]["start"] : speech_timestamps[-1]["end"]]
+                ),
                 metadata={"sample_rate": sr},
             )
-
-            for i, ts in enumerate(reversed(speech_timestamps)):
-                # Convert timestamps to milliseconds
-                speech_timestamps[i]["start"] = int(ts["start"] / sr * 1000)
-                speech_timestamps[i]["end"] = int(ts["end"] / sr * 1000)
-                if (ts["end"] - ts["start"]) > MIN_SPEECH_DURATION_MS:
-                    arg_max = len(speech_timestamps) - i
-                    break
-            # Check ig there is timestamp
-            if (
-                len(speech_timestamps) > 0
-                and len(
-                    audio[speech_timestamps[0]["start"] : speech_timestamps[-1]["end"]]
-                )
-                > MIN_AUDIO_SAMPLING_DURATION_MS * sr / 1000
-                and (
-                    (len(audio) - speech_timestamps[arg_max]["end"])
-                    > MIN_SILENCE_DURATION_MS / 1000 * sr
-                )
-            ):
-                # Check if the audio is not cut at the end. And only return if there is a long time spent
-                if speech_timestamps[-1]["end"] == len(audio):
-                    node.send_output(
-                        "timestamp_start",
-                        pa.array([speech_timestamps[-1]["start"]]),
-                        metadata={"sample_rate": sr},
-                    )
-                audio = audio[: speech_timestamps[-1]["end"]]
-                node.send_output("audio", pa.array(audio), metadata={"sample_rate": sr})
-                last_audios = [audio[speech_timestamps[-1]["end"] :]]
-
-            # If there is no sound for too long return the audio
-            elif len(last_audios) > MAX_AUDIO_DURATION_S:
-                node.send_output("audio", pa.array(audio), metadata={"sample_rate": sr})
-                last_audios = []
+            last_audios = []
