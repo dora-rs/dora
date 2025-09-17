285c4436ac41252a16caaa538149a7a9fc22d7f5
Philipp Oppermann <dev@phil-opp.com>
2025-09-17 17:09:05 +0200

    Add back cuda-benchmark example

---
+++ examples/cuda-benchmark/README.md
@@ -0,0 +1,44 @@
+# Example of latency using CUDA Zero Copy instead of regular Shared Memory over CPU
+
+## Install
+
+```bash
+pip install dora-rs-cli # if not already present
+
+# Install pyarrow with gpu support
+conda install pyarrow "arrow-cpp-proc=*=cuda" -c conda-forge
+
+## Test installation with
+python -c "import pyarrow.cuda"
+
+# Install numba for translation from arrow to torch
+pip install numba
+
+## Test installation with
+python -c "import numba.cuda"
+
+# Install torch if it's not already present
+pip install torch
+
+## Test installation with
+python -c "import torch; assert torch.cuda.is_available()"
+```
+
+## Run
+
+```bash
+dora run cpu_bench.yml
+
+dora run cuda_bench.yml
+
+cat benchmark_data.csv
+```
+
+## To run the demo code
+
+```bash
+dora up
+dora start demo_bench.yml --detach
+python demo_receiver.py
+dora destroy
+```
---
+++ examples/cuda-benchmark/demo_receiver.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+"""TODO: Add docstring."""
+
+import os
+import time
+
+import numpy as np
+import pyarrow as pa
+import torch
+from dora import Node
+from dora.cuda import ipc_buffer_to_ipc_handle, open_ipc_handle
+from helper import record_results
+from tqdm import tqdm
+
+torch.tensor([], device="cuda")
+
+
+pa.array([])
+node = Node("node_2")
+
+current_size = 8
+n = 0
+i = 0
+latencies = []
+mean_cpu = mean_cuda = 0
+DEVICE = os.getenv("DEVICE", "cuda")
+
+NAME = f"dora torch {DEVICE}"
+
+print()
+print("Receiving 40MB packets using default dora-rs")
+
+while True:
+    event = node.next()
+
+    if event["type"] == "INPUT":
+        if i == 0:
+            pbar = tqdm(total=100)
+        elif i == 100:
+            print("vs")
+            print("Receiving 40MB packets using dora-rs CUDA->CUDA")
+            pbar = tqdm(total=100)
+        t_send = event["metadata"]["time"]
+
+        if event["metadata"]["device"] != "cuda":
+            # BEFORE
+            handle = event["value"].to_numpy()
+            scope = None
+            torch_tensor = torch.tensor(handle, device="cuda")
+        else:
+            # AFTER
+            ipc_handle = ipc_buffer_to_ipc_handle(event["value"], event["metadata"])
+            scope = open_ipc_handle(ipc_handle, event["metadata"])
+            torch_tensor = scope.__enter__()
+    else:
+        break
+    t_received = time.perf_counter_ns()
+    length = len(torch_tensor) * 8
+
+    pbar.update(1)
+    latencies.append((t_received - t_send) / 1000)
+    node.send_output("next", pa.array([]))
+
+    i += 1
+    if i == 100:
+        pbar.close()
+        t_end_cpu = time.time()
+        mean_cpu = np.array(latencies).mean()
+        latencies = []
+    n += 1
+
+    if scope:
+        scope.__exit__(None, None, None)
+
+
+mean_cuda = np.array(latencies).mean()
+pbar.close()
+
+time.sleep(2)
+
+print()
+print("----")
+print(f"Node communication duration with default dora-rs: {mean_cpu / 1000:.1f}ms")
+print(f"Node communication duration with dora CUDA->CUDA: {mean_cuda / 1000:.1f}ms")
+
+print("----")
+print(f"Speed Up: {(mean_cpu) / (mean_cuda):.0f}")
+record_results(NAME, current_size, latencies)
