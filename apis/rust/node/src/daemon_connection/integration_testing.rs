5988d12496b7cbeff4d6c4b1caba855cd917de85
Philipp Oppermann <dev@phil-opp.com>
2025-10-15 17:22:16 +0200

    Improve output and input file formats

--- apis/rust/node/src/daemon_connection/integration_testing.rs
+++ apis/rust/node/src/daemon_connection/integration_testing.rs
@@ -1,6 +1,11 @@
-use std::{fs::File, path::PathBuf};
+use std::{
+    fs::File,
+    path::PathBuf,
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
-use arrow::array::RecordBatch;
+use arrow::array::{Array, Float64Array, RecordBatch, StringArray};
 use arrow_json::LineDelimitedWriter;
 use colored::Colorize;
 use dora_core::{
@@ -26,6 +31,7 @@ pub struct IntegrationTestingEvents {
     events: std::vec::IntoIter<TimedInputEvent>,
     output_file: File,
     start_timestamp: uhlc::Timestamp,
+    start_time: Instant,
 }
 
 impl IntegrationTestingEvents {
@@ -41,15 +47,17 @@ impl IntegrationTestingEvents {
         node_info
             .events
             .as_mut_slice()
-            .sort_by_key(|i| i.time_offset);
+            .sort_by(|a, b| a.time_offset_secs.total_cmp(&b.time_offset_secs));
         let inputs = std::mem::take(&mut node_info.events).into_iter();
 
         let clock = HLC::default();
         let start_timestamp = clock.new_timestamp();
+        let start_time = Instant::now();
         Ok(Self {
             events: inputs,
             output_file,
             start_timestamp,
+            start_time,
         })
     }
 
@@ -71,14 +79,29 @@ impl IntegrationTestingEvents {
                 metadata,
                 data,
             } => {
-                let (drop_tx, drop_rx) = flume::unbounded();
-                let array = data_to_arrow_array(data.clone(), metadata, drop_tx)
-                    .context("failed to convert output to arrow array")?;
-                // integration testing doesn't use shared memory -> no drop tokens
-                let _ = drop_rx;
+                let mut fields = Vec::new();
 
-                let batch = RecordBatch::try_from_iter([(output_id, array)])
-                    .context("failed to create RecordBatch")?;
+                let id: Arc<dyn Array> =
+                    Arc::new(StringArray::from_iter([Some(output_id.as_str())]));
+                fields.push(("id", id));
+
+                if data.is_some() {
+                    let (drop_tx, drop_rx) = flume::unbounded();
+                    let data_array = data_to_arrow_array(data.clone(), metadata, drop_tx)
+                        .context("failed to convert output to arrow array")?;
+                    // integration testing doesn't use shared memory -> no drop tokens
+                    let _ = drop_rx;
+                    fields.push(("data", data_array));
+                }
+
+                let time_offset = metadata
+                    .timestamp()
+                    .get_diff_duration(&self.start_timestamp);
+                let time_offset_secs = Float64Array::from_iter_values([time_offset.as_secs_f64()]);
+                fields.push(("time_offset_secs", Arc::new(time_offset_secs)));
+
+                let batch =
+                    RecordBatch::try_from_iter(fields).context("failed to create RecordBatch")?;
 
                 let mut writer = LineDelimitedWriter::new(&mut self.output_file);
                 writer
@@ -122,11 +145,21 @@ impl IntegrationTestingEvents {
             return Ok(None);
         };
 
-        let TimedInputEvent { time_offset, event } = event;
+        let TimedInputEvent {
+            time_offset_secs,
+            event,
+        } = event;
+        let time_offset = Duration::from_secs_f64(time_offset_secs);
+        let elapsed = self.start_time.elapsed();
+        if let Some(wait_time) = time_offset.checked_sub(elapsed) {
+            std::thread::sleep(wait_time);
+        }
+
         let timestamp = Timestamp::new(
             self.start_timestamp.get_time() + NTP64::from(time_offset),
             *self.start_timestamp.get_id(),
         );
+
         let converted = match event {
             InputEvent::Stop => NodeEvent::Stop,
             InputEvent::Input { id, metadata, data } => {
