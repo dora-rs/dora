d241cb902be0ce463e27b9b6fece2ae1b6759be8
Philipp Oppermann <dev@phil-opp.com>
2025-11-19 15:26:43 +0100

    Use env variable instead of introducing new CLI args

--- binaries/cli/src/command/start/mod.rs
+++ binaries/cli/src/command/start/mod.rs
@@ -5,7 +5,7 @@
 use super::{Executable, default_tracing};
 use crate::{
     command::start::attach::attach_dataflow,
-    common::{connect_to_coordinator, local_working_dir, resolve_dataflow},
+    common::{connect_to_coordinator, local_working_dir, resolve_dataflow, write_events_to},
     output::print_log_message,
     session::DataflowSession,
 };
@@ -53,9 +53,6 @@ pub struct Start {
     // Use UV to run nodes.
     #[clap(long, action)]
     uv: bool,
-    /// Path to write dataflow events to as JSON lines (for debugging).
-    #[clap(long)]
-    write_events_to: Option<PathBuf>,
 }
 
 impl Executable for Start {
@@ -63,13 +60,8 @@ impl Executable for Start {
         default_tracing()?;
         let coordinator_socket = (self.coordinator_addr, self.coordinator_port).into();
 
-        let (dataflow, dataflow_descriptor, mut session, dataflow_id) = start_dataflow(
-            self.dataflow,
-            self.name,
-            coordinator_socket,
-            self.uv,
-            self.write_events_to,
-        )?;
+        let (dataflow, dataflow_descriptor, mut session, dataflow_id) =
+            start_dataflow(self.dataflow, self.name, coordinator_socket, self.uv)?;
 
         let attach = match (self.attach, self.detach) {
             (true, true) => eyre::bail!("both `--attach` and `--detach` are given"),
@@ -116,7 +108,6 @@ fn start_dataflow(
     name: Option<String>,
     coordinator_socket: SocketAddr,
     uv: bool,
-    write_events_to: Option<PathBuf>,
 ) -> Result<(PathBuf, Descriptor, Box<TcpRequestReplyConnection>, Uuid), eyre::Error> {
     let dataflow = resolve_dataflow(dataflow).context("could not resolve dataflow")?;
     let dataflow_descriptor =
@@ -141,7 +132,7 @@ fn start_dataflow(
                     name,
                     local_working_dir,
                     uv,
-                    write_events_to,
+                    write_events_to: write_events_to(),
                 })
                 .unwrap(),
             )
