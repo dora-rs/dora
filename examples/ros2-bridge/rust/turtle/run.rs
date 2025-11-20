cf046ce63edbdd31ae2972a9bcae5f05681e03b1
drindr <dreamchancn@qq.com>
2025-11-20 22:28:36 +0800

    refactor: place the ros2-bridge example standalone
    place the rust ros2 example standalone
    add ros2 rust example align with ros2's official minimal example
    
    Signed-off-by: drindr <dreamchancn@qq.com>

---
+++ examples/ros2-bridge/rust/turtle/run.rs
@@ -0,0 +1,37 @@
+use dora_cli::{build, run};
+use eyre::Context;
+use std::path::Path;
+
+use process_wrap::std::{ChildWrapper, CommandWrap, ProcessGroup};
+
+fn main() -> eyre::Result<()> {
+    let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+    std::env::set_current_dir(root.join("../../../").join(file!()).parent().unwrap())
+        .wrap_err("failed to set working dir")?;
+
+    build("dataflow.yml".to_string(), None, None, false, true)?;
+
+    let dataflow_task = std::thread::spawn(|| {
+        run("dataflow.yml".to_string(), false).unwrap();
+    });
+
+    let mut add_service_task = run_ros_node("examples_rclcpp_minimal_service", "service_main")?;
+    let mut turtle_task = run_ros_node("turtlesim", "turtlesim_node")?;
+
+    while !dataflow_task.is_finished() {}
+
+    add_service_task.kill()?;
+    turtle_task.kill()?;
+    Ok(())
+}
+
+fn run_ros_node(package: &str, node: &str) -> eyre::Result<Box<dyn ChildWrapper>> {
+    let mut command = CommandWrap::with_new("ros2", |cmd| {
+        cmd.arg("run");
+        cmd.arg(package).arg(node);
+    });
+    command.wrap(ProcessGroup::leader());
+    command
+        .spawn()
+        .map_err(|e| eyre::eyre!("failed to spawn ros node: {}", e))
+}
