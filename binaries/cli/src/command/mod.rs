f55c6dc7d6a63136fc413dc4e8685ea2ad5340e6
drindr <dreamchancn@qq.com>
2025-11-25 17:20:46 +0800

    Add completions subcommand
    With this command, it can generate auto-completion
    script for specific shell, like bash and fish.
    
    Use `eval "$(dora completions <SHELL>)"` to apply
    the autocompletion script
    
    Signed-off-by: drindr <dreamchancn@qq.com>

--- binaries/cli/src/command/mod.rs
+++ binaries/cli/src/command/mod.rs
@@ -1,5 +1,6 @@
 mod build;
 mod check;
+mod completions;
 mod coordinator;
 mod daemon;
 mod destroy;
@@ -20,6 +21,7 @@ pub use run::{run, run_func};
 
 use build::Build;
 use check::Check;
+use completions::Completions;
 use coordinator::Coordinator;
 use daemon::Daemon;
 use destroy::Destroy;
@@ -64,6 +66,7 @@ pub enum Command {
     #[clap(subcommand)]
     Topic(Topic),
 
+    Completions(Completions),
     Self_ {
         #[clap(subcommand)]
         command: SelfSubCommand,
@@ -106,6 +109,7 @@ impl Executable for Command {
             Command::Self_ { command } => command.execute(),
             Command::Runtime(args) => args.execute(),
             Command::Topic(args) => args.execute(),
+            Command::Completions(args) => args.execute(),
         }
     }
 }
