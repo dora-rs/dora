e3603a32a5df325c3a4c2d783a907162c86c711a
haixuantao <tao.xavier@outlook.com>
2025-12-12 18:44:07 +0800

    fix opentelemetry library

--- examples/rust-dataflow/node/src/main.rs
+++ examples/rust-dataflow/node/src/main.rs
@@ -1,22 +1,43 @@
-use dora_node_api::{self, DoraNode, Event, EventStream, IntoArrow, dora_core::config::DataId};
+use std::sync::LazyLock;
+
+use dora_node_api::{
+    self, DoraNode, Event, EventStream, IntoArrow, dora_core::config::DataId, init_tracing,
+};
 
 #[cfg(test)]
 mod tests;
-
+use tokio::runtime::{Builder, Runtime};
+use tracing::{Level, span, warn};
+static RUNTIME: LazyLock<Runtime> = LazyLock::new(|| {
+    Builder::new_multi_thread()
+        .worker_threads(2)
+        .enable_all()
+        .build()
+        .unwrap()
+});
 fn main() -> eyre::Result<()> {
     println!("hello");
 
     let (node, events) = DoraNode::init_from_env()?;
+    let id = node.id().clone();
+    let dataflow_id = node.dataflow_id().clone();
+    RUNTIME.spawn(async move {
+        let guard = init_tracing(&id, &dataflow_id).unwrap();
+        loop {
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        }
+    });
 
     run(node, events)
 }
 
 fn run(mut node: DoraNode, mut events: EventStream) -> eyre::Result<()> {
+    let span = span!(Level::INFO, "Node Span");
+    let _enter = span.enter();
     // use a fixed seed for reproducibility (we use this node's output in integration tests)
     fastrand::seed(42);
 
     let output = DataId::from("random".to_owned());
-
     for i in 0..100 {
         let event = match events.recv() {
             Some(input) => input,
@@ -27,15 +48,21 @@ fn run(mut node: DoraNode, mut events: EventStream) -> eyre::Result<()> {
             Event::Input { id, metadata, data } => match id.as_str() {
                 "tick" => {
                     let random: u64 = fastrand::u64(..);
-                    println!("tick {i} with data {data:?}, sending {random:#x}");
-                    node.send_output(output.clone(), metadata.parameters, random.into_arrow())?;
+                    warn!("tick {i} with data {data:?}, sending {random:#x}");
+                    node.send_output(output.clone(), metadata.parameters, random.into_arrow())
+                        .unwrap();
                 }
-                other => eprintln!("Ignoring unexpected input `{other}`"),
+                other => {
+                    tracing::warn!(input = other, "Ignoring unexpected input");
+                }
             },
-            Event::Stop(_) => println!("Received stop"),
-            other => eprintln!("Received unexpected input: {other:?}"),
+            Event::Stop(_) => {
+                tracing::info!("Received stop event");
+            }
+            other => {
+                tracing::warn!(?other, "Received unexpected input");
+            }
         }
     }
-
     Ok(())
 }
