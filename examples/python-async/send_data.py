b0d4f7adba02af0d99ba24337999f01387b3381a
Philipp Oppermann <dev@phil-opp.com>
2025-12-23 09:27:29 +0100

    Revert "Make adding more `NodeEvent` variants backwards-compatible" (#1277)
    Revert "Make adding more `NodeEvent` variants backwards-compatible (#1273)"
    
    This reverts commit f2381503f601cf5d15186020c15395d2d59c73d8.

--- examples/python-async/send_data.py
+++ examples/python-async/send_data.py
@@ -1,15 +1,12 @@
 """TODO: Add docstring."""
 
 import time
-import logging
 
 import numpy as np
 import pyarrow as pa
 from dora import Node
 
 node = Node()
-
-log = logging.getLogger(__name__)
 
 i = 0
 for event in node:
@@ -18,5 +15,4 @@ for event in node:
     else:
         i += 1
     now = time.perf_counter_ns()
-    logging.warning(f"sending {i}: {now}")
     node.send_output("data", pa.array([np.uint64(now)]))
