8313dc829a9f281645a1c8c4a0a6eea9545e21d3
Harsh Sahu <harshsahu4375@gmail.com>
2026-02-17 06:09:25 +0530

    feat(cli) : add system doctor command with shared memory feature

--- binaries/cli/src/command/system/mod.rs
+++ binaries/cli/src/command/system/mod.rs
@@ -1,20 +1,24 @@
+pub mod doctor;
 pub mod status;
 
 pub use status::check_environment;
 
 use super::Executable;
+use doctor::Doctor;
 use status::Status;
 
 /// System management commands
 #[derive(Debug, clap::Subcommand)]
 pub enum System {
     Status(Status),
+    Doctor(Doctor),
 }
 
 impl Executable for System {
     fn execute(self) -> eyre::Result<()> {
         match self {
             System::Status(args) => args.execute(),
+            System::Doctor(args) => args.execute(),
         }
     }
 }
